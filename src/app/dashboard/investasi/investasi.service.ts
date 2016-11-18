cef7656cbb5d42f6e114a0c32743da3664a2c758
ari_fakhri <arifakhri97@gmail.com>
2016-11-18 16:34:14 +0700

    make file investasi service for post data

---
+++ src/app/dashboard/investasi/investasi.service.ts
@@ -0,0 +1,18 @@
+import { Injectable }		from '@angular/core';
+import { Headers, Http }	from '@angular/http';
+import { Observable }     	from 'rxjs/Observable';
+
+@Injectable ()
+export class InvestasiService {
+	private headers  = new Headers ({'Content-Type' : 'application/json'}); //URL to web API
+	private createinvestasi = 'https://private-f1c97-masscredit.apiary-mock.com/mobile/user/investloan/new'; //URL API Create Investasi
+
+	constructor (private http:Http) { }
+
+	private data = {
+		
+	}
+	CreateInvestasi(data) {
+		// code...
+	}
+}
