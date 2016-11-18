4e97c9232e9e8bcb70f4527a3578b5cbcd8cdf61
ari_fakhri <arifakhri97@gmail.com>
2016-11-18 16:33:23 +0700

    make partial create investasi

---
+++ src/app/dashboard/investasi/create/create_investasi.component.ts
@@ -0,0 +1,9 @@
+import { Component } 	from '@angular/core';
+
+@Component({
+	moduleId: module.id,
+	selector: 'create-investasi',
+	templateUrl: 'create_investasi.component.html'
+})
+
+export class CreateInvestasiComponent { }
