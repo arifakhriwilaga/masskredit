ea9dd7cf8aee69477c4279d0ebd836384c913481
ari.fakhri@appschef.com <ari.fakhri@appschef.com>
2016-11-23 13:13:56 +0700

    add styleUrls for css base auth

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,17 +1,21 @@
 import { Component }       from '@angular/core';
 import { ActivatedRoute }  from '@angular/router';
-import { XLargeAuth } from './x-large';
+import { XLargeAuth }      from './x-large';
+import '../../assets/css/main.css';
 
 @Component({
   // The selector is what angular internally uses
   // for `document.querySelectorAll(selector)` in our index.html
   // where, in this case, selector is the string 'home'
+  // moduleId: module.id, //component for styleUrls (Masih gagal)
   selector: 'auth',  // <auth></lauth>
   // We need to tell Angular's Dependency Injection which providers are in our app.
   // providers: [
   //   Title
   // ],
   // Every Angular template is first compiled by the browser before Angular runs it's compiler
-  templateUrl: './auth.component.html'
+  templateUrl: './auth.component.html',
+  // styleUrls: ['./auth.component.css'] //component for styleUrls (Masih gagal)
+
 })
 export class AuthComponent { }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,14 @@
 import { Component }       from '@angular/core';
 import { ActivatedRoute }  from '@angular/router';
-
 import { XLarge } from './x-large';
+import '../../assets/css/main.css';
+
 
 @Component({
   // The selector is what angular internally uses
   // for `document.querySelectorAll(selector)` in our index.html
   // where, in this case, selector is the string 'home'
+  // moduleId: module.id, //component for styleUrls (Masih gagal)
   selector: 'home',  // <home></home>
   // We need to tell Angular's Dependency Injection which providers are in our app.
   // providers: [
@@ -14,5 +16,7 @@ import { XLarge } from './x-large';
   // ],
   // Every Angular template is first compiled by the browser before Angular runs it's compiler
   templateUrl: './home.component.html',
-})
+  // styleUrls: ['./home.component.css'] //component for styleUrls (Masih gagal)
+  }
+)
 export class HomeComponent { }
