bc38bbaf5fa18721232c5d7083674b6370854eef
ari.fakhri@appschef.com <ari.fakhri@appschef.com>
2016-12-05 13:06:38 +0700

    delete wrong code

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,6 +2,8 @@ import { Component, ViewEncapsulation }    from '@angular/core';
 import { ActivatedRoute }                  from '@angular/router';
 
 
+declare var jQuery : any;
+
 @Component({
 
   // //moduleId: module.id,
@@ -11,4 +13,22 @@ import { ActivatedRoute }                  from '@angular/router';
   // styles: [require('./home.component.css')] //component for styleUrls (Masih gagal)
   // encapsulation: ViewEncapsulation.None,
 })
-export class HomeComponent { }
+
+
+export class HomeComponent { 
+
+
+	ngOnInit(){
+		// nav
+      jQuery("document").ready(function($){
+      var headNav = $('.navbar-head');
+       $(window).scroll(function () {
+           if ($(this).scrollTop() > 40) {
+               headNav.addClass("fixed");
+           } else {
+               headNav.removeClass("fixed");
+           } 
+       });
+      });
+	}
+}
