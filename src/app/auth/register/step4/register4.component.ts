c847375fa3f96e483ba0399dc693cf6d912223d1
ari.fakhri@appschef.com <ari.fakhri@appschef.com>
2016-12-05 12:58:47 +0700

    delete comment code

--- src/app/auth/register/step4/register4.component.ts
+++ src/app/auth/register/step4/register4.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } 				from '@angular/core';
 import { RegisterService }					from './../register.service';
 import { Headers, Http, RequestOptions }	from '@angular/http';
 import { FileUploader }						from 'ng2-file-upload';
+import { Router }  								from '@angular/router';
+
 
 declare var jQuery : any;
 
@@ -12,55 +14,41 @@ const URL = 'https://evening-anchorage-3159.herokuapp.com/api/';
 	templateUrl: 'register4.component.html'
 })
 
-export class Step4RegisterComponent implements OnInit {
+export class Step4RegisterComponent {
 	constructor(
 		private registerService: RegisterService, 
 		private http: Http,
+		private router : Router
 		// private uploader: FileUploader
 	) {	}
 
 
 
-	private headers  	= new Headers ({'Content-Type' : 'application/json'}); //URL to web API
 	private register = this.registerService.dataRegister();
 	private a;
 	private b;
 	private c;
 
-
-
-	
-	// getFoto(event) {
-	// 		var foto_ktp_depan: any = event.target || event.srcElement;
-	// 		console.log(foto_ktp_depan.files);
-	// 		// console.log(d)
-	// 		// return x;
-	// }
-
-	ngOnInit(){
-
-		// console.log(this.register);
-		// return this.register;
-	}
-
 	stepFinish() {
 
-		// let readerFileA = new FileReader();
-		// readerFileA.onload = function(event, varty) {
-		// 	this.a = event.target.result.split(',')[1];
-		// 	console.log(this.a);
-		// }.bind(this);
+		let readerFileA = new FileReader();
+		readerFileA.onload = function(event, varty) {
+			this.a = event.target.result.split(',')[1];
+			debugger
+			// console.log(this.a);
+		}.bind(this);
 
 		let readerFileB = new FileReader();
 		readerFileB.onload = function(event, varty) {
 			this.b = event.target.result.split(',')[1];
-			console.log(this.b)
+			debugger
+			// console.log(this.b)
 		}.bind(this)
 
 		let readerFileC = new FileReader();
 		readerFileC.onload = function(event, varty) {
 			this.c = event.target.result.split(',')[1];
-			console.log(this.c)
+			// console.log(this.c)
 		}.bind(this)
 
 		let x : any = document.getElementById("foto_ktp_depan");
@@ -70,64 +58,47 @@ export class Step4RegisterComponent implements OnInit {
 		var file_y =	y.files[0];
 		var file_z =	z.files[0];
 
-		// var encode_x  = readerFileA.readAsDataURL(file_x);
+		console.log(file_x, file_y, file_z);
+		var encode_x  = readerFileA.readAsDataURL(file_x);
 		var encode_y  = readerFileB.readAsDataURL(file_y);
 		var encode_z  = readerFileC.readAsDataURL(file_z);
-		
-		// console.log(encode_x,encode_y,encode_z);
-		// console.log(encode_x);
-		
-		// var file_x   = x.files[0];
+		this.register.foto_ktp_depan	= encode_x;
+		this.register.foto_ktp_belakang = encode_y;
+		this.register.foto_diri			= encode_z;
 
-		// console.log(encode_x);
-		// consol
-		// var decode_x = atob(encode_x);
-		// var decode_x = atob(string_x);
-		// console.log(encode_x);
-		// console.log([x.files[0],y.files[0],z.files[0]]);
-		// this.register.foto_ktp_depan	= JSON.stringify(x.files[0]);
-		// this.register.foto_ktp_belakang = JSON.stringify(y.files[0]);
-		// this.register.foto_diri			= JSON.stringify(z.files[0]);
-		let headers = new Headers({ 'Content-Type': 'application/json' });
-    	let options = new RequestOptions({ headers: headers });
+		this.register.access_token 		= localStorage.getItem("access_code");
 
+		console.log(this.register);
 
+		// Set Headers
+		let headers = new Headers({ 
+		 	'Content-Type': 'application/json',
+		 	'api_key' : '01b19716dfe44d0e9c656903429c3e9c65d0b243' 
+	 	});
 
-		// this.register;
-		// console.log(this.register)
-			// this.http.post('http://masscredit-backend.stagingapps.net:9000/user/credential/register',
-			// 	this.register, 
-			// 	options)
-			// .map(response => response.json())
-			// .subscribe((response : any) => {
-			// 	console.log(response);
-				// var token 	 = data.json();
-				// localStorage.setItem('access_token', JSON.stringify(token.data.access_token));
-				// debugger;
-				// console.log(token.meta.code,token.meta.message, user);
-				// if(token.meta.code == "200") {
-					// return this.router.navigateByUrl('dashboard');
-				// }
-				// else{
-					// return this.router.navigateByUrl('/')
-				// }
-			// });
+	    let options = new RequestOptions({ headers: headers });
+
+		//API Register 
+		this.register;
+		console.log(this.register)
+			this.http.post('http://masscredit-backend.stagingapps.net:9000/user/credential/register',
+			this.register, 
+			options)
+			.map(response => response.json())
+			.subscribe((response : any) => {
+				var message 	= response.meta.message;
+				var code 		= response.meta.code;
+				
+				console.log(message);
+				if(code == "200") {
+					localStorage.removeItem("access_code");
+					return this.router.navigateByUrl('/');
+				}
+				else{
+					return this.router.navigateByUrl('/auth/register/step-1')
+				}
+			});
 	}
-
-	// ngAfterViewInit(){
-	// 	function readURL(input){
-	// 		if(input.files && input.filse[0]) {
-	// 			var reader = new FileReader();
-	// 			reader.onload = function(e) {
-	// 				jQuery('#foto_ktp_depan')
-	// 				.attr('src', e.target)
-	// 				.width(150)
-	// 				.height(200);
-	// 			};
-	// 			reader.readAsDataURL(input.files[0]);
-	// 		}
-	// 	}
-	// }
 	
 	prevStepThree(){
 		console.log();
