bd487dfc9c15a4f93a5e8677fcb0e5629a5c1999
ari_fakhri <ari.fakhri@appschef.com>
2016-12-12 06:26:41 +0700

    make route and view detail pinjaman

--- src/app/dashboard/pinjaman/index/index.component.ts
+++ src/app/dashboard/pinjaman/index/index.component.ts
@@ -13,16 +13,17 @@ import { Router }        					   from '@angular/router';
 export class IndexLoanComponent { 
 	constructor(private http: Http) { }
 
-	public pinjaman = [];
 
 	ngOnInit(){
-		this.listInvestasi();
+		this.listPinjaman();
 			
 	}
 
 	// private acces_token = localStorage.getItem("access_token");
-    public token = { access_token : JSON.parse(localStorage.getItem("access_token")) };
-	listInvestasi() {
+    public token = {access_token : JSON.parse(localStorage.getItem("access_token"))} ;
+	public pinjaman = [];
+	listPinjaman() {
+		console.log("Sedang mengambil data....")
 		let headers = new Headers({ 
 		 	'Content-Type': 'application/json',
 		 	'api_key' : '01b19716dfe44d0e9c656903429c3e9c65d0b243' 
@@ -30,28 +31,28 @@ export class IndexLoanComponent {
 
 	    let options = new RequestOptions({ headers: headers });
 
-
-
-	    console.log(this.token);	    
-
-	    // Get data Agama
 		this.http.post('http://masscredit-api.stagingapps.net/user/investment/getlist',this.token,options)
 				.map(response => response.json())
 				.subscribe((response : any) => {
-					// for message
-					var code 		= response.meta.code;
-					var message 	= response.meta.message;
-			
 					console.log(response);
-					console.log(code,message);
+					// for message
+					// var code 		= response.meta.code;
+					// var message 	= response.meta.message;
+					this.pinjaman = response.data.investments;
+				
+					// console.log(code,message);
 					// get data
 
-					this.pinjaman 	= response.data.investments;
-					console.log("Pinjaman",this.pinjaman)
+					// this.pinjaman 	= response.data.investments;
+					// console.log("Pinjaman",this.pinjaman)
 
 				
 				});
 		// return this.investasiservice.Index();
+	}
+
+	getDetail(value){
+		console.log(value);
 	}
 
 }
