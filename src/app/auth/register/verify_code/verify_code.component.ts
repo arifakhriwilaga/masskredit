3eb0699aec1a4e8114d5940f0848cfbc2e810cf5
ari_fakhri <ari.fakhri@appschef.com>
2016-12-12 06:18:14 +0700

    make validation on verify_code

--- src/app/auth/register/verify_code/verify_code.component.ts
+++ src/app/auth/register/verify_code/verify_code.component.ts
@@ -3,6 +3,8 @@ import { Router }											from '@angular/router';
 import { Headers, Http, RequestOptions }					from '@angular/http';
 import { FormGroup, FormBuilder, Validators, FormControl }  from '@angular/forms';
 import { ValidationServiceVerify } 							from './validation/validationservice.component'
+import { RegisterService }									from './../register.service';
+import 'rxjs/add/operator/catch';
 
 declare var jQuery:any;
 
@@ -14,43 +16,63 @@ declare var jQuery:any;
 })
 
 export class VerifyCodeComponent {
-	constructor(private router : Router, private http : Http) { }
+	verifyCodeForm: FormGroup;
+	constructor(private router : Router, private http : Http, private formBuilder: FormBuilder,
+		private registerService: RegisterService, 
+	) {
+		this.verifyCodeForm = this.formBuilder.group({
+			'verification_code' : [null, Validators.required]
+		})
+	}
 
 	private code = {
 		verification_code : '',
 
 	};
 	// send code verify
-	sendVerify(code)  {
-		let headers = new Headers({ 
-			'Content-Type': 'application/json',
-			'api_key' : '01b19716dfe44d0e9c656903429c3e9c65d0b243'
-		});
+	sendVerify(code:any)  {
 		
-    	let options = new RequestOptions({ headers: headers });
-
-		console.log(code);
-		if(code) {
-		this.http.post('http://masscredit-api.stagingapps.net/user/credential/verifycode',code, options)
-		.map( data => data.json() )
-		.subscribe((data) => {
-			console.log(data)
-			localStorage.removeItem("verify_handphone");
-			localStorage.setItem('access_code', data.data.access_token);
-			// console.log(localStorage.getItem("access_code"));
-			this.router.navigateByUrl('/auth/register/step-1');
-
-
-			
-		});
-
-		}
-		else{
+		if(code == null) {
+			alert("Code Verifikasi anda salah");
 			console.log('Gagal Verify');
+		}
+
+		else{
+			let headers = new Headers({ 
+				'Content-Type': 'application/json',
+				'api_key' : '01b19716dfe44d0e9c656903429c3e9c65d0b243'
+			})
+		
+	    	let options = new RequestOptions({ headers: headers });
+
+			console.log(code);
+			return this.http.post('http://masscredit-api.stagingapps.net/user/credential/verifycode',code, options)
+			.map( (data) => data.json() )
+			.subscribe((data) => {
+				console.log("Verify Success..");
+				localStorage.removeItem("verify_handphone");
+				localStorage.setItem('access_code', data.data.access_code);
+				// console.log(localStorage.getItem("access_code"));
+				this.router.navigateByUrl('/auth/register/step-1');
+			});
 		}
 	}
 
+	// HandleError
+	// private handleError(error : any){
+	// 	let errMsg: string;
+	// 	if(error instanceof any) {
+			 
+	// 	}
+	// }
+
+	private register = this.registerService.dataRegister();
 	cancelVerify(){
+	
+
+
+
+		console.log("Verify dibatalkan")
 		localStorage.removeItem("verify_handphone");
 		this.router.navigateByUrl("");
 	}
