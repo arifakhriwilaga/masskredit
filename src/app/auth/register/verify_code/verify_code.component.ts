c714ab81db7a054a4c8f34d740196d812cc762bc
ari_fakhri <ari.fakhri@appschef.com>
2016-12-20 08:01:09 +0700

    change validator FormBuilder to jquery validator in verify_code.component

--- src/app/auth/register/verify_code/verify_code.component.ts
+++ src/app/auth/register/verify_code/verify_code.component.ts
@@ -17,13 +17,26 @@ declare var jQuery:any;
 
 export class VerifyCodeComponent {
 	verifyCodeForm: FormGroup;
-	constructor(private router : Router, private http : Http, private formBuilder: FormBuilder,
-		private registerService: RegisterService, 
-	) {
-		this.verifyCodeForm = this.formBuilder.group({
-			'verification_code' : [null, Validators.required]
-		})
-	}
+	constructor(
+		private router			: Router, 
+		private http 			: Http,
+		private registerService	: RegisterService, 
+	) {	}
+
+	ngAfterViewInit() {
+		jQuery(function($){
+			jQuery('#verification_code').mask('00000');
+		});
+		jQuery( "#verifyCodeForm" ).validate({
+		  rules: {
+		    verification_code: {
+		      required: true
+		    }
+		  }
+		});
+        
+		// jQuery('#verifikasi-modal').modal('show');
+    }
 
 	private code = {
 		verification_code : '',
@@ -32,12 +45,7 @@ export class VerifyCodeComponent {
 	// send code verify
 	sendVerify(code:any)  {
 		
-		if(code == null) {
-			alert("Code Verifikasi anda salah");
-			console.log('Gagal Verify');
-		}
-
-		else{
+		if(jQuery("#verifyCodeForm").valid()) {
 			let headers = new Headers({ 
 				'Content-Type': 'application/json',
 				'api_key' : '01b19716dfe44d0e9c656903429c3e9c65d0b243'
@@ -50,31 +58,55 @@ export class VerifyCodeComponent {
 			.map( (data) => data.json() )
 			.subscribe((data) => {
 				console.log("Verify Success..");
-				localStorage.removeItem("verify_handphone");
+				// localStorage.removeItem("verify_handphone");
 				localStorage.setItem('access_code', data.data.access_code);
 				// console.log(localStorage.getItem("access_code"));
 				this.router.navigateByUrl('/auth/register/step-1');
 			});
 		}
+
+		else{
+			alert("Code Verifikasi anda salah");
+			console.log('Gagal Verify');
+		}
 	}
-
-	// HandleError
-	// private handleError(error : any){
-	// 	let errMsg: string;
-	// 	if(error instanceof any) {
-			 
-	// 	}
-	// }
-
+	private nomor = {
+		phone_number : JSON.stringify(localStorage.getItem("verify_handphone"))
+	};
 	private register = this.registerService.dataRegister();
-	cancelVerify(){
-	
+	// private getNomor =;
+	resendHandphone()  {
+			let headers = new Headers({ 
+				'Content-Type': 'application/json',
+				'api_key' : '01b19716dfe44d0e9c656903429c3e9c65d0b243'
+			});
+	    	let options = new RequestOptions({ headers: headers });
+			
+			console.log("Sedang mengirim data....");
 
 
+				this.http.post('http://masscredit-api.stagingapps.net/user/credential/getverificationcode',
+				this.nomor,
+				options)
+				.map(data => data.json())
+				.subscribe((data : any) => {
+					var verify 	= JSON.stringify(this.nomor)
+					var code 	= data.data.verification_code;
+					// var header 	= data;
+					// console.log(header.meta);
+					console.log("Verify code :",code)
+					// // alert(code);
+					// // console.log(data);
+					// localStorage.setItem("verify_handphone", verify)
+					// this.router.navigateByUrl("/auth/register/verify-code") // return view verify code for now
+				
+				});
+			
+			
+		
+			console.log("This valid");
+			// code...
 
-		console.log("Verify dibatalkan")
-		localStorage.removeItem("verify_handphone");
-		this.router.navigateByUrl("");
 	}
 
 }
