087f9788ba19110e6cf286b16b54863f8256f811
ari_fakhri <ari.fakhri@appschef.com>
2016-12-21 15:05:14 +0700

    change validation FormBuilder to jquery validator

--- src/app/dashboard/investasi/create/create.component.ts
+++ src/app/dashboard/investasi/create/create.component.ts
@@ -5,62 +5,69 @@ import { Headers, Http, RequestOptions }	   from '@angular/http';
 import { Router }        					   from '@angular/router';
 
 declare var jQuery:any;
-// declare var image:void;
+declare var image:void;
 
 @Component({
 	selector: 'create',
 	templateUrl: 'create.component.html',
 })
 
-export class CreateInvestComponent { 
-	createForm : any;
-	constructor(
-		private http: Http, 
-		private router:Router,
-		private formBuilder: FormBuilder
-	) { 
-
-	this.createForm = this.formBuilder.group({
-		'access_token' 	: JSON.parse(localStorage.getItem("access_token")),
-		'invest_name'	: ['', [Validators.required, ValidationServiceInvestasi.namainvestasiValidator]],
-		'type'			: [0, Validators.required],
-		'images1'		: [""],
-		'images2'		: [""],
-		'images3'		: [""],
-		'due_date'		: [''],
-		'amount'		: ['', [Validators.required, ValidationServiceInvestasi.amountValidator]],
-		'interest'		: [''],
-		'tenor'			: ['', [Validators.required, ValidationServiceInvestasi.tenorValidator]],
-		'collateral'	: ['', [Validators.required]],
-		'description'	:  '',
-      	
-
-      	// 'email': ['', [Validators.required, ValidationService.emailValidator]],
-      	// 'profile': ['', [Validators.required, Validators.minLength(10)]]
-    });
-
-	}
+export class CreateInvestComponent {
+	constructor(private http: Http, private router:Router) { }
 
 	
 
 	ngOnInit() {
 		jQuery('.datepicker').datepicker({
-	      format	: 'YYYY-MM-DD',
+	      format	: 'yyyy/mm/dd',
 	      // startDate : '2015-01-01',
 	      // minDate	: '01/01/2015'
+
 	    });
+	    jQuery( "#investasiForm" ).validate({
+		  rules: {
+		    invest_name: {
+		      required: true
+		    },
+		    type: {
+		      required: true,
+		      // email	  : true
+		    },
+		    images1: {
+		      required: true
+		    },
+		    masa_berlaku: {
+		      required: true
+		    },
+		    amount: {
+		      required: true
+		    },
+		    tenor: {
+		      required: true
+		    },
+		    collateral: {
+		      required: true
+		    },
+		    description: {
+		      required: true
+		    },
+		  }
+		});
 	}
 
 	private image:void;
 
 	private token = JSON.parse(localStorage.getItem("access_token"));
 		
+	// objek investasi
 	public investasi = {
 		access_token: this.token,
 	  	invest_name: 'My invest',
 	  	type: '0',
 	  	description: 'Motor',
-		images: "",
+		images1: this.image,
+		images2: this.image,
+		images3: this.image,
 	  	due_date: '',
 	  	amount: null,
 	  	interest: null,
@@ -72,95 +79,90 @@ export class CreateInvestComponent {
 
 
 
-  // createInvestasi(investasi) {
-  // 	if(!investasi.value.valid || investasi.value == "") {
-  // 		jQuery.click();
-  // 		this.router.navigateByUrl('/dashboard/invest/create');		
-  // 	}
-  // 	else{
-	 //  	console.log(investasi);
-  // 	}
-
-  // }
 
   	createInvestasi(investasi) {
-  		// this.investasi = investasi;
-  		investasi.due_date = jQuery("#due_date").datepicker("getDate");
-  		// console.log(investasi);
-  		
-  		// debugger
-  	let readerFileA = new FileReader();
-  		
-		readerFileA.onload = function(event, varty) {
-			let fileA = event.target.result.split(',')[1];
-			investasi.images1 = fileA;
-			
-			// console.log(fileA);
-		}.bind(this);
+		if(jQuery("#investasiForm").valid()) {	
+	  		// this.investasi = investasi;
+	  		this.investasi.due_date = jQuery("#masa_berlaku").datepicker("getDate");
+	  		// console.log(investasi);
+	  		
+	  		// debugger
+	  		let readerFileA = new FileReader();
+	  		
+			readerFileA.onload = function(event, varty) {
+				let fileA = event.target.result.split(',')[1];
+				this.investasi.images1 = fileA;
+				console.log(fileA);
+			}.bind(this);
 
-		let readerFileB = new FileReader();
-		readerFileB.onload = function(event, varty) {
-			let fileB = event.target.result.split(',')[1];
-			investasi.images2 = fileB;
-			
-			// console.log(fileB)
-		}.bind(this)
-
-		let readerFileC = new FileReader();
-		readerFileC.onload = function(event, varty) {
-			let fileC = event.target.result.split(',')[1];
-			investasi.images3 = fileC;
-
-			// console.log(fileC)
-
-		  	let headers = new Headers({ 
-					'Content-Type': 'application/json',
-					'api_key' : '01b19716dfe44d0e9c656903429c3e9c65d0b243'
-				});
-		    	let options = new RequestOptions({ headers: headers });
+			let readerFileB = new FileReader();
+			readerFileB.onload = function(event, varty) {
+				let fileB = event.target.result.split(',')[1];
+				this.investasi.images2 = fileB;
 				
-				console.log(investasi);
+				// console.log(fileB)
+			}.bind(this)
 
-				if(investasi == null) {
-					console.log('Gagal membuat investasi');
+			let readerFileC = new FileReader();
+			readerFileC.onload = function(event, varty) {
+				let fileC = event.target.result.split(',')[1];
+				this.investasi.images3 = fileC;
 
-				}
-				else{
+				console.log(this.investasi)
+
+			  	let headers = new Headers({ 
+						'Content-Type': 'application/json',
+						'api_key' : '01b19716dfe44d0e9c656903429c3e9c65d0b243'
+					});
+		    	let options = new RequestOptions({ headers: headers });
+					
 					this.http.post('http://masscredit-api.stagingapps.net/user/investment/new',
 					investasi,
 					options)
 					.map(response => response.json())
-					.subscribe((response : any) => {
-					var code 		= response.meta.code;
-					var message 	= response.meta.message;
-					
-					console.log(code,message);
+					.subscribe(
+						(response : any) => {
+							var code 		= response.meta.code;
+							var message 	= response.meta.message;
+							
+							console.log(code,message);
 
-					this.router.navigateByUrl('/dashboard/pinjaman');
-					
-					});
-				}
+							this.router.navigateByUrl('/dashboard/pinjaman');
+						},
+						(err:any) => {
+							var error   = JSON.parse(err._body)
+							var message = error.meta.message
+								if(message == "unauthorized") {
+									alert("Maaf session anda telah habis silahkan login kembali")
+									return this.router.navigateByUrl('/dashboard/sign-out')
+									
+								}	
+						}
+					);				
+			}.bind(this)
+
+			// var reader = new FileReader();
+			console.log(this.investasi)
+
+			let x : any = document.getElementById("image1");
+			let y : any = document.getElementById("image2");
+			let z : any = document.getElementById("image3");
+			var file_x =	x.files[0];
+			var file_y =	y.files[0];
+			var file_z =	z.files[0];
+
+			// debugger
 			
-		}.bind(this)
-
-		// var reader = new FileReader();
-
-		let x : any = document.getElementById("image1");
-		let y : any = document.getElementById("image2");
-		let z : any = document.getElementById("image3");
-		var file_x =	x.files[0];
-		var file_y =	y.files[0];
-		var file_z =	z.files[0];
-
-		// debugger
-		
-		var encode_x  = readerFileA.readAsDataURL(file_x);
-		var encode_y  = readerFileB.readAsDataURL(file_y);
-		var encode_z  = readerFileC.readAsDataURL(file_z);
-		investasi.images1	= encode_x;
-		investasi.images2 	= encode_y;
-		investasi.images3	= encode_z;
-
+			var encode_x  = readerFileA.readAsDataURL(file_x);
+			var encode_y  = readerFileB.readAsDataURL(file_y);
+			var encode_z  = readerFileC.readAsDataURL(file_z);
+			this.investasi.images1	= encode_x;
+			this.investasi.images2 	= encode_y;
+			this.investasi.images3	= encode_z;
+		}
+		else{
+			alert("Data tidak valid");
+		}
   		
   }
 
