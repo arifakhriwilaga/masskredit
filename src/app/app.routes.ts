546c4f4a4db51f08469641a3f58e040f9d6959c3
ari_fakhri <ari.fakhri@appschef.com>
2016-12-21 15:11:29 +0700

    add guard in route dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes, RouterModule } from '@angular/router';
 import { NoContentComponent }   from './no-content';
 import { DataResolver }         from './app.resolver';
+// Guard Dashboard
+import { AuthGuardEntryDashboard }   from './authguard/auth-guard-entry-dashboard.service';
 
 // Verify Email
 import { VerifyEmailComponent }          from './verify_email';
@@ -10,7 +12,11 @@ export const ROUTES: Routes = [
   // { path: 'coba', component: CobaComponent },
   { path: 'home', 	   loadChildren: () => System.import('./home').then((comp: any) => comp.default), },
   { path: 'auth', 	   loadChildren: () => System.import('./auth').then((comp: any) => comp.default), },
-  { path: 'dashboard', loadChildren: () => System.import('./dashboard').then((comp: any) => comp.default), },
+  { 
+	canActivate : [ AuthGuardEntryDashboard ],
+  	path: 'dashboard', 
+  	loadChildren: () => System.import('./dashboard').then((comp: any) => comp.default) 
+  },
   { path: 'verify-email/:access_code', component: VerifyEmailComponent },
 
 ];
