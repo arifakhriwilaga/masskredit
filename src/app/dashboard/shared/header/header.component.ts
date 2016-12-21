39a4843125380bbdf216af3775e989d07b2c2a16
ari_fakhri <ari.fakhri@appschef.com>
2016-12-21 15:01:25 +0700

    make new objek for avg_review and fund_history

--- src/app/dashboard/shared/header/header.component.ts
+++ src/app/dashboard/shared/header/header.component.ts
@@ -40,14 +40,31 @@ export class HeaderComponent {
       let options = new RequestOptions({ headers: headers });
       this.http.post('http://masscredit-api.stagingapps.net/user/credential/profile',token,options)
         .map(response => response.json())
-        .subscribe((response : any) => {
-          // console.log(response);
-          this.name         	= response.data.profile.name;
-          this.last_login       = response.data.profile.last_login;
-          this.user_class       = response.data.profile.user_class;
-          this.profile_image    = response.data.profile.profile_image;
-          this.account_summary 	= response.data.account_summary.balance;
-        });
+        .subscribe(
+	        (response : any) => {
+	          console.log(response);
+	          this.name         	= response.data.profile.name;
+	          this.last_login       = response.data.profile.last_login;
+	          this.user_class       = response.data.profile.user_score.user_class;
+	          this.user_status		= response.data.profile.user_score.user_status;
+	          this.profile_image    = response.data.profile.profile_image;
+	          this.account_summary 	= response.data.account_summary.balance;
+	          this.investor 		= response.data.profile.user_score.investor;
+	          this.borrower 		= response.data.profile.user_score.borrower;
+	          this.avg_reviews 		= response.data.profile.user_score.avg_reviews;
+	          this.fund_history 	= response.data.profile.user_score.fund_history;
+
+	        },
+		(err:any) => {
+            var error   = JSON.parse(err._body)
+            var message = error.meta.message
+              if(message == "unauthorized") {
+                alert("Maaf session anda telah habis silahkan login kembali")
+                // return this.router.navigateByUrl('/dashboard/sign-out')
+                
+              }  
+        }
+        );
 	
 	// let data        = { };
 	// this.profile 	= data; 
@@ -55,14 +72,18 @@ export class HeaderComponent {
  //  	let account_summary = {};
   	
  // 		this.profile;
-	console.log(this.name)
+	// console.log(this.name)
  	}	
  	
 	private name 			= { };
 	private last_login 		= { };
 	private user_class 		= { };
+	private user_status 	= { };
 	private profile_image 	= { };
-
+	private investor		= { };
+	private borrower 		= { };
+	private avg_reviews		= { };
+	private fund_history	= { };
 	private account_summary = { };
 
 	// console.log(this.profile)
