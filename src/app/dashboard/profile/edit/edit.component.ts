d9fcaf833077e14203614b8b6500fef1009ce20a
ari_fakhri <ari.fakhri@appschef.com>
2016-12-29 16:05:43 +0700

    make validation update some foto in profile but still fail

--- src/app/dashboard/profile/edit/edit.component.ts
+++ src/app/dashboard/profile/edit/edit.component.ts
@@ -69,14 +69,20 @@ export class EditComponent {
 		sumber_pendapatan_lain_2 	: null,
 		jumlah_pendapatan_lain_2 	: this.number,
 		// step4
-		foto_identitas		: this.image,
-		foto_npwp 		  	: this.image,
+		foto_identitas		: null,
+		foto_npwp 		  	: null,
 		foto_diri	 	  	: null,
 
 		// objek data bank
-		foto_tabungan		: this.image,
+		foto_tabungan		: null,
 		no_rekening			: ''
 	}
+
+	// public copy_data = {
+	// 	copy_foto_identitas : null,
+	// 	copy_foto_npwp : null,
+	// 	copy_foto_identitas : null,
+	// }
 
 
 	ngOnInit(){
@@ -213,7 +219,8 @@ export class EditComponent {
 	}
 
 	updateProfile(data){
-		console.log(data);
+
+		// console.log(data);
 		if(this.data.sumber_pendapatan == 1) {
 			this.data.nama_usaha 	  				= "";
 			this.data.tahun_perusahaan_berdiri 		= "";
@@ -236,140 +243,130 @@ export class EditComponent {
 		this.data.access_token 	= this.acces_token;
 		// console.log(data)
 
-		// let readerFileA = new FileReader();
-		// readerFileA.onload = function(event, varty) {
-		// 	let fileA = event.target.result.split(',')[1];
-		// 	this.data.foto_tabungan = fileA;
-		// 	this.editProfile();
-		// 	// console.log(fileB)
-		// }.bind(this)
+		let readerFileA = new FileReader();
+		readerFileA.onload = function(event, varty) {
+			let fileA = event.target.result.split(',')[1];
+			if(fileA == "AQID") {
+				this.data.foto_tabungan = null
+			}
+			else{
+				return this.data.foto_tabungan = fileA;
+			}
+			// console.log(fileB)
+		}.bind(this)
 
-		// let readerFileX = new FileReader();
+		let readerFileX = new FileReader();
   		
-		// readerFileX.onload = function(event, varty) {
-		// 	let fileX = event.target.result.split(',')[1];
-		// 	this.data.foto_identitas = fileX;
-		// 	this.editProfile();
-		// 	// console.log(fileA);
-		// }.bind(this);
+		readerFileX.onload = function(event, varty) {
+			let fileX = event.target.result.split(',')[1];
+			if(fileX == "AQID") {
+				this.data.foto_identitas = null
+			}
+			else{
+				return this.data.foto_identitas = fileX;
+			}
+			// console.log(fileA);
+		}.bind(this);
 
-		// let readerFileY = new FileReader();
-		// readerFileY.onload = function(event, varty) {
-		// 	let fileY = event.target.result.split(',')[1];
-		// 	this.data.foto_npwp = fileY;
-		// 	this.editProfile();
-		// 	// console.log(fileB)
-		// }.bind(this)
+		let readerFileY = new FileReader();
+		readerFileY.onload = function(event, varty) {
+			let fileY = event.target.result.split(',')[1];
+			if(fileY == "AQID") {
+				this.data.foto_npwp = null
+			}
+			else{
+				return this.data.foto_npwp = fileY;
+			}
+			
+			
+			// console.log(fileB)
+		}.bind(this)
 
 
 		let readerFileZ = new FileReader();
 		readerFileZ.onload = function(event, varty) {
 			let fileZ = event.target.result.split(',')[1];
-		
-					this.data.foto_diri = fileZ;
-
-					console.log(data);
-					console.log("Sedang mengirim data....")
-					let headers = new Headers({ 
-					 	'Content-Type': 'application/json',
-					 	'api_key' : '01b19716dfe44d0e9c656903429c3e9c65d0b243' 
-				 	});
-
-				    let options = new RequestOptions({ headers: headers });
-
-					this.http.put('http://masscredit-api.stagingapps.net/user/credential/update-profile',this.data,options)
-							.map(response => response.json())
-							.subscribe(
-								(response : any) => {
-									// for message
-									var kosong:null;
-									var code 		= response.meta.code;
-									var message 	= response.meta.message;
-									if(code == 200) {
-										alert("Profile berhasil diupate")
-										return this.router.navigateByUrl('/dashboard/profile')
-									}
-									else{
-										alert("Profile gagal diupdate")
-									}
-								},
-								(err:any) => {
-						            var error   = JSON.parse(err._body)
-						            var message = error.meta.message
-						              if(message == "unauthorized") {
-						                alert("Maaf session anda telah habis silahkan login kembali")
-						                return this.router.navigateByUrl('/dashboard/sign-out')
-						                
-						              } 
-						        }
-							);
-		
-
-		}.bind(this);
-
-
-
-			// let a : any = document.getElementById("foto_tabungan");
-			// let x : any = document.getElementById("foto_identitas");
-			// let y : any = document.getElementById("foto_npwp");
-			let z : any = document.getElementById("foto_diri");
-			
-			// var file_a =	a.files[0];
-			// var file_x =	x.files[0];
-			// var file_y =	y.files[0];
-			var file_z =	z.files[0];
-
-			if(file_z != undefined) {
-				// var encode_a  = readerFileA.readAsDataURL(file_a);
-				// var encode_x  = readerFileX.readAsDataURL(file_x);
-				// var encode_y  = readerFileY.readAsDataURL(file_y);
-				var encode_z  = readerFileZ.readAsDataURL(file_z);
-				// this.data.foto_tabungan		= encode_a;
-				// this.data.foto_identitas	= encode_x;
-				// this.data.foto_npwp 		= encode_y;
-				this.data.foto_diri			= encode_z;
-			}
-			else{
-				this.data.foto_diri 	= null;
-				this.data.foto_identitas= null;
-				this.data.foto_npwp		= null;
-				this.data.foto_tabungan = null;
-				console.log(data)
+				if(fileZ == "AQID") {
+				this.data.foto_diri = null
+				}
+				if(fileZ != "AQID") {
+				this.data.foto_diri = fileZ;
+				}
+				
+				console.log(this.data);
+				console.log("Sedang mengirim data....")
+				
 				let headers = new Headers({ 
 				 	'Content-Type': 'application/json',
 				 	'api_key' : '01b19716dfe44d0e9c656903429c3e9c65d0b243' 
 			 	});
 
 			    let options = new RequestOptions({ headers: headers });
-				this.http.put('http://masscredit-api.stagingapps.net/user/credential/update-profile',this.data,options)
-				.map(response => response.json())
-				.subscribe(
-					(response : any) => {
-						// for message
-						var kosong:null;
-						var code 		= response.meta.code;
-						var message 	= response.meta.message;
-						if(code == 200) {
-							alert("Profile berhasil diupate")
-							return this.router.navigateByUrl('/dashboard/profile')
-						}
-						else{
-							alert("Profile gagal diupdate")
-						}
-					},
-					(err:any) => {
-			            var error   = JSON.parse(err._body)
-			            var message = error.meta.message
-			              if(message == "unauthorized") {
-			                alert("Maaf session anda telah habis silahkan login kembali")
-			                return this.router.navigateByUrl('/dashboard/sign-out')
-			                
-			              } 
-			        }
-				)
-				// this.changeProfile();
-		}
 
+				this.http.put('http://masscredit-api.stagingapps.net/user/credential/update-profile',this.data,options)
+						.map(response => response.json())
+						.subscribe(
+							(response : any) => {
+								// for message
+								var kosong:null;
+								var code 		= response.meta.code;
+								var message 	= response.meta.message;
+								if(code == 200) {
+									alert("Profile berhasil diupate")
+									return this.router.navigateByUrl('/dashboard/profile')
+								}
+								else{
+									alert("Profile gagal diupdate")
+								}
+							},
+							(err:any) => {
+					            var error   = JSON.parse(err._body)
+					            var message = error.meta.message
+					              if(message == "unauthorized") {
+					                alert("Maaf session anda telah habis silahkan login kembali")
+					                return this.router.navigateByUrl('/dashboard/sign-out')
+					                
+					              } 
+					        }
+						);
+
+		}.bind(this)
+
+
+
+			let a : any = document.getElementById("foto_tabungan");
+			let x : any = document.getElementById("foto_identitas");
+			let y : any = document.getElementById("foto_npwp");
+			let z : any = document.getElementById("foto_diri");
+			
+			var file_a =	a.files[0];
+			var file_x =	x.files[0];
+			var file_y =	y.files[0];
+			var file_z =	z.files[0];
+			var objectBlob	= new Uint8Array([1,2,3]);
+			var arrayBlob	= objectBlob.buffer;
+			var image_default = new Blob([arrayBlob]);
+
+			if(file_z == undefined) {
+				file_z = image_default;
+			}
+			if(file_y == null) {
+				file_y = image_default;
+			}
+			if(file_x == undefined) {
+				file_x = image_default;
+			}
+			if(file_a == undefined) {
+				file_a = image_default;
+			}
+			var encode_a  = readerFileA.readAsDataURL(file_a);
+			var encode_x  = readerFileX.readAsDataURL(file_x);
+			var encode_y  = readerFileY.readAsDataURL(file_y);
+			var encode_z  = readerFileZ.readAsDataURL(file_z);
+			this.data.foto_tabungan		= encode_a;
+			this.data.foto_identitas	= encode_x;
+			this.data.foto_npwp 		= encode_y;
+			this.data.foto_diri			= encode_z;
 	}
 
 	private statusMarried:any;
@@ -458,38 +455,44 @@ export class EditComponent {
 		}
 	}
 
-	// changeProfile(){
-	// 	let headers = new Headers({ 
-	// 	 	'Content-Type': 'application/json',
-	// 	 	'api_key' : '01b19716dfe44d0e9c656903429c3e9c65d0b243' 
-	//  	});
+	changeProfile(){
+		let headers = new Headers({ 
+		 	'Content-Type': 'application/json',
+		 	'api_key' : '01b19716dfe44d0e9c656903429c3e9c65d0b243' 
+	 	});
 
-	// 	this.http.put('http://masscredit-api.stagingapps.net/user/credential/update-profile',this.data,options)
-	// 						.map(response => response.json())
-	// 						.subscribe(
-	// 							(response : any) => {
-	// 								// for message
-	// 								var kosong:null;
-	// 								var code 		= response.meta.code;
-	// 								var message 	= response.meta.message;
-	// 								if(code == 200) {
-	// 									alert("Profile berhasil diupate")
-	// 									return this.router.navigateByUrl('/dashboard/profile')
-	// 								}
-	// 								else{
-	// 									alert("Profile gagal diupdate")
-	// 								}
-	// 							},
-	// 							(err:any) => {
-	// 					            var error   = JSON.parse(err._body)
-	// 					            var message = error.meta.message
-	// 					              if(message == "unauthorized") {
-	// 					                alert("Maaf session anda telah habis silahkan login kembali")
-	// 					                return this.router.navigateByUrl('/dashboard/sign-out')
+	 	let options = new RequestOptions({ headers: headers });
+
+		this.http.put('http://masscredit-api.stagingapps.net/user/credential/update-profile',this.data,options)
+							.map(response => response.json())
+							.subscribe(
+								(response : any) => {
+									// for message
+									var kosong:null;
+									var code 		= response.meta.code;
+									var message 	= response.meta.message;
+									if(code == 200) {
+										alert("Profile berhasil diupate")
+										return this.router.navigateByUrl('/dashboard/profile')
+									}
+									else{
+										alert("Profile gagal diupdate")
+									}
+								},
+								(err:any) => {
+						            var error   = JSON.parse(err._body)
+						            var message = error.meta.message
+						              if(message == "unauthorized") {
+						                alert("Maaf session anda telah habis silahkan login kembali")
+						                return this.router.navigateByUrl('/dashboard/sign-out')
 						                
-	// 					              } 
-	// 					        }
-	// 						);
-	// }
+						              } 
+						        }
+							);
+	}
+
+	cancelUpdateProfile(){
+		return this.router.navigateByUrl('/dashboard/profile')
+	}
 
 }
