647ac4c7a92fc33cd61b052c04bfb93eaf420f15
ari_fakhri <ari.fakhri@appschef.com>
2017-01-09 15:04:28 +0700

    make directory for display component bank in complement_user

---
+++ src/app/dashboard/complement_user/bank/bank.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } 	from '@angular/core';
+import { ComplementComponent }	from './../index';
+
+declare var jQuery:any;
+
+@Component({
+	//moduleId: module.id,
+	selector: 'bank',
+	templateUrl: 'bank.component.html'
+})
+
+export class BankComponent { 
+
+	constructor(private complement:ComplementComponent) {
+	// initial objek in complement data
+	this.data = this.complement.data;
+		
+	}
+
+	ngOnInit(){
+		jQuery('.datepicker').datepicker({
+	      format	: 'yyyy-mm-dd',
+	      // startDate : '2015-01-01',
+	      // minDate	: '01/01/2015'
+
+	    });
+	}
+	public data = { }
+	// public data = {
+
+	// 	// objek pekerjaan
+	// 	nama_perusahaan 	  : '',
+	// 	mulai_bekerja 	  	  : '',
+	// 	jabatan  	 		  : '',
+	// 	pekerjaan  	 		  : '',
+	// 	gaji_per_bulan 		  : 0,
+	// 	pengeluaran_per_bulan : '',
+	// 	tlp_perusahaan		  : ''
+		
+		
+
+	// 		// counter++;
+	// }
+
+}
