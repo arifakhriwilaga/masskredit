dacf3aa1549821bac30e70382fee667a148a0e15
ari_fakhri <ari.fakhri@appschef.com>
2017-01-10 16:35:39 +0700

    make directory installment in folder complement_user, and contain [service, component, view]

---
+++ src/app/dashboard/complement_user/installment/installment.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } 	from '@angular/core';
+import { ComplementComponent }	from './../complement.component';
+
+declare var jQuery:any;
+
+@Component({
+	//moduleId: module.id,
+	selector: 'installment',
+	templateUrl: 'installment.component.html'
+})
+
+export class InstallmentComponent { 
+
+	constructor(private complement:ComplementComponent) {
+	// initial objek in complement data
+	this.data = this.complement.data;
+		
+	}
+
+	ngOnInit(){
+		jQuery('.datepicker').datepicker({
+	      format	: 'yyyy-mm-dd',
+	      // startDate : '2015-01-01',
+	      // minDate	: '01/01/2015'
+
+	    });
+	}
+	public data = { }
+	// public data = {
+
+	// 	// objek pekerjaan
+	// 	nama_perusahaan 	  : '',
+	// 	mulai_bekerja 	  	  : '',
+	// 	jabatan  	 		  : '',
+	// 	pekerjaan  	 		  : '',
+	// 	gaji_per_bulan 		  : 0,
+	// 	pengeluaran_per_bulan : '',
+	// 	tlp_perusahaan		  : ''
+		
+		
+
+	// 		// counter++;
+	// }
+
+}
