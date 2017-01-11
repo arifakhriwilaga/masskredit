17997abd59e123114db97351293e5ac06ad39f82
ari_fakhri <ari.fakhri@appschef.com>
2017-01-11 13:07:20 +0700

    delete object data and function ngOnInit() in bank.component

--- src/app/dashboard/complement_user/bank/bank.component.ts
+++ src/app/dashboard/complement_user/bank/bank.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } 	from '@angular/core';
-import { ComplementComponent }	from './../index';
+import { ComplementComponent }	from './../complement.component';
+
+import { Headers, Http, RequestOptions }	   from '@angular/http';
 
 declare var jQuery:any;
 
@@ -11,35 +13,52 @@ declare var jQuery:any;
 
 export class BankComponent { 
 
-	constructor(private complement:ComplementComponent) {
+	constructor(private complement:ComplementComponent, private http : Http) {
 	// initial objek in complement data
 	this.data = this.complement.data;
 		
 	}
 
+	// objek for save data bang when success get data bank on API
+	private banks = [];
+
+	private headers = new Headers({ 
+			 	'Content-Type': 'application/json',
+			 	'api_key' : '01b19716dfe44d0e9c656903429c3e9c65d0b243' 
+		 	});
+
+	private options = new RequestOptions({ headers: this.headers });
+
 	ngOnInit(){
-		jQuery('.datepicker').datepicker({
-	      format	: 'yyyy-mm-dd',
-	      // startDate : '2015-01-01',
-	      // minDate	: '01/01/2015'
-
-	    });
+		// Request for get data bank
+		this.http.get('http://masscredit-api.stagingapps.net/master/bank',this.options)
+			.map(response => response.json())
+			.subscribe(
+				(response : any) => {
+					var code 		= response.meta.code;
+					var message 	= response.meta.message;
+					var banks 		= response.data.tipe_bank;
+					this.banks 		= banks;
+					console.log(response);
+					// this.router.navigateByUrl('/dashboard/fund');
+				},
+				(err:any) => {
+					var error   = JSON.parse(err._body)
+					var message = error.meta.message
+						// if(message == "unauthorized") {
+						// 	alert("Maaf session anda telah habis silahkan login kembali")
+						// 	return this.router.navigateByUrl('/dashboard/sign-out')
+							
+						// }	
+				}
+			);			
+		
 	}
+
 	public data = { }
-	// public data = {
 
-	// 	// objek pekerjaan
-	// 	nama_perusahaan 	  : '',
-	// 	mulai_bekerja 	  	  : '',
-	// 	jabatan  	 		  : '',
-	// 	pekerjaan  	 		  : '',
-	// 	gaji_per_bulan 		  : 0,
-	// 	pengeluaran_per_bulan : '',
-	// 	tlp_perusahaan		  : ''
-		
-		
-
-	// 		// counter++;
-	// }
-
+	// get id bank name
+	getBankName(id){
+		this.complement.data.bank = id;
+	}
 }
