26e8f37ff7259ac7542da3e3b3226aa4d872b875
ari_fakhri <ari.fakhri@appschef.com>
2017-01-16 08:35:01 +0700

    make function for condition if some image doesnt value in edit profile

--- src/app/dashboard/profile/edit/edit.component.ts
+++ src/app/dashboard/profile/edit/edit.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } 		from '@angular/core';
 import { Headers, Http, RequestOptions }	   from '@angular/http';
 import { Router }        					   from '@angular/router';
+import {Observable} from 'rxjs/Observable';
 
 declare var jQuery:any;
 @Component({
@@ -92,8 +93,10 @@ export class EditComponent implements OnInit {
 		// objek data bank
 		public foto_tabungan		= null;
 		public check_image = [];
+		// public setEncodeImage = setTimeout(this.encodeImages, 3000)
 
 	ngOnInit(){
+	
 		// this.fileReader.onload = function(event, varty) {
 		// 	let fileA = event.target.result.split(',')[1];
 		// 	if(fileA == "AQID") {
@@ -152,6 +155,7 @@ export class EditComponent implements OnInit {
     public sumber_pendapatan = { }
 	public complement_data = { };
 	public profile 		   = { };
+
 	getProfile() {
 		console.log("Sedang mengambil data....")
 		let headers = new Headers({ 
@@ -241,190 +245,94 @@ export class EditComponent implements OnInit {
 
 	updateProfile(data){
 
-		if(this.data.sumber_pendapatan == 2) {
-			this.data.nama_perusahaan 	= "";
-			this.data.mulai_bekerja 	= "";
-			this.data.jabatan  	 		= "";
-			this.data.pekerjaan  	 	= "";
-			this.data.gaji_per_bulan 	= 0;
+	if(data) {
+		this.encodeImages();
+		
+	}
+	// alert("after image encodes")
 
-		}
+	// try {
+	// 	// alert("dari try")
+	// 	if(this.data.sumber_pendapatan == 2) {
+	// 		this.data.nama_perusahaan 	= "";
+	// 		this.data.mulai_bekerja 	= "";
+	// 		this.data.jabatan  	 		= "";
+	// 		this.data.pekerjaan  	 	= "";
+	// 		this.data.gaji_per_bulan 	= 0;
+	// 	}
 
-		if(this.data.status_perkawinan == 2) {
-			this.data.jumlah_anak		= 0;
-		}
+	// 	if(this.data.status_perkawinan == 2) {
+	// 		this.data.jumlah_anak		= 0;
+	// 	}
 
-		 // get value mulai_bekerja
-		let value_date = jQuery("#mulai_bekerja").val();
-		this.data.mulai_bekerja = value_date;
+	// 	 // get value mulai_bekerja
+	// 	let value_date = jQuery("#mulai_bekerja").val();
+	// 	this.data.mulai_bekerja = value_date;
 
-		// if(this.data.sumber_pendapatan) {
-		// 	this.data.status_perkawinan = null;
-		// 	this.data.jumlah_anak		= null;
-		// 	this.data.jumlah_tanggungan = null;
-		// }
+	// 	// if(this.data.sumber_pendapatan) {
+	// 	// 	this.data.status_perkawinan = null;
+	// 	// 	this.data.jumlah_anak		= null;
+	// 	// 	this.data.jumlah_tanggungan = null;
+	// 	// }
 					
-		this.data.access_token 	= this.acces_token;
-		// console.log(data)
+	// 	this.data.access_token 	= this.acces_token;
+	// 	// console.log(data)
 
-		let readerFileA = new FileReader();
-		readerFileA.onload = function(event, varty) {
-			let fileA = event.target.result.split(',')[1];
-			if(fileA == "AQID") {
-				this.data.foto_tabungan = null
-			}
-			else{
-				// this.data.foto_tabungan = fileA;
-				this.foto_tabungan = fileA;
-			}
-			this.check_image.push(5)
-			// console.log(fileB)
-		}.bind(this)
+	// 	// if(this.check_image.length == 4) {
+	// 	// alert("dari finally")
+	// 	// console.log(this.check_image)
+	// 	// this.data.foto_identitas = this.foto_identitas
+	// 	// this.data.foto_diri		 = this.foto_diri
+	// 	// this.data.foto_tabungan	 = this.foto_tabungan
+	// 	// this.data.foto_npwp		 = this.foto_npwp
 
-		let readerFileX = new FileReader();
-  		
-		readerFileX.onload = function(event, varty) {
-			alert("dari file x")
-			let fileX = event.target.result.split(',')[1];
-			// console.log(fileX)
-			if(fileX == "AQID") {
-				this.data.foto_identitas = null
-			}
-			else{
-				// alert("dari else")
-				// this.data.foto_identitas = fileX;
-				this.foto_identitas = fileX;
-				// console.log(this.foto_identitas)
-			}
-				this.check_image.push(3)
-				console.log(this.check_image)
-			return;
-			// console.log(fileA);
-		}.bind(this);
-
-		let readerFileY = new FileReader();
-		readerFileY.onload = function(event, varty) {
-			let fileY = event.target.result.split(',')[1];
-			if(fileY == "AQID") {
-				this.data.foto_npwp = null
-			}
-			else{
-				this.foto_npwp = fileY;
-				// this.data.foto_npwp = fileY;
+	// 		alert("Dari finally	")
 				
-			}
-			this.check_image.push(2)
-			return;
-			
-			// console.log(fileB)
-		}.bind(this)
+	// 			let headers = new Headers({ 
+	// 			 	'Content-Type': 'application/json',
+	// 			 	'api_key' : '01b19716dfe44d0e9c656903429c3e9c65d0b243' 
+	// 		 	});
 
+	// 		    let options = new RequestOptions({ headers: headers });
 
-		let readerFileZ = new FileReader();
-		readerFileZ.onload = function(event, varty) {
-			// let dari_file_x = this.foto_identitas
-			// console.log(dari_file_x)
-			let fileZ = event.target.result.split(',')[1];
-				if(fileZ == "AQID") {
-				this.data.foto_diri = null
-				this.check_image.push(1)
-				}
-				if(fileZ != "AQID") {
-				this.data.foto_diri = fileZ;
-				this.check_image.push(1)
-				// this.data.foto_diri = fileZ;
-				}
-
-				// if(this.check_image == ['4']) {
-				// 	alert("dari check image!")
-				// 	console.log(this.data)
-				// 	// console.log(this.check_image)
-				// 	// code...
-				// }
-				// console.log(this.check_image)
-				// this.check_image.push(4)
-				// return	;
-				// this.check()
-				// self(this.check()
-				// EditComponent.call(this.changeProfile())
-				// this.foto_identitas = this.data.foto_identitas;
-				// 	console.log(this.data);
-				// // let image = this.data_image;
-				// this.data_image.foto_diri = this.data.foto_diri
-				// // this.data_image.foto_tabungan = this.data.foto_tabungan
-				// // this.data_image.foto_npwp = this.data.foto_npwp
-				// // image.foto_identitas = this.data.foto_identitas
-				// // console.log(this.data);
-				// console.log("Sedang mengirim data....")
-				
-				let headers = new Headers({ 
-				 	'Content-Type': 'application/json',
-				 	'api_key' : '01b19716dfe44d0e9c656903429c3e9c65d0b243' 
-			 	});
-
-			    let options = new RequestOptions({ headers: headers });
-
-				this.http.put('http://masscredit-api.stagingapps.net/user/credential/update-profile',this.data,options)
-						.map(response => response.json())
-						.subscribe(
-							(response : any) => {
-								// for message
-								var kosong:null;
-								var code 		= response.meta.code;
-								var message 	= response.meta.message;
-								if(code == 200) {
-									alert("Profile berhasil diupate")
-									return this.router.navigateByUrl('/dashboard/profile')
-								}
-								else{
-									alert("Profile gagal diupdate")
-								}
-							},
-							(err:any) => {
-					            var error   = JSON.parse(err._body)
-					            var message = error.meta.message
-					              if(message == "unauthorized") {
-					                alert("Maaf session anda telah habis silahkan login kembali")
-					                return this.router.navigateByUrl('/dashboard/sign-out')
+	// 			// this.http.put('http://masscredit-api.stagingapps.net/user/credential/update-profile',this.data,options)
+	// 			// 		.map(response => response.json())
+	// 			// 		.subscribe(
+	// 			// 			(response : any) => {
+	// 			// 				// for message
+	// 			// 				var kosong:null;
+	// 			// 				var code 		= response.meta.code;
+	// 			// 				var message 	= response.meta.message;
+	// 			// 				if(code == 200) {
+	// 			// 					alert("Profile berhasil diupate")
+	// 			// 					return this.router.navigateByUrl('/dashboard/profile')
+	// 			// 				}
+	// 			// 				else{
+	// 			// 					alert("Profile gagal diupdate")
+	// 			// 				}
+	// 			// 			},
+	// 			// 			(err:any) => {
+	// 			// 	            var error   = JSON.parse(err._body)
+	// 			// 	            var message = error.meta.message
+	// 			// 	              if(message == "unauthorized") {
+	// 			// 	                alert("Maaf session anda telah habis silahkan login kembali")
+	// 			// 	                return this.router.navigateByUrl('/dashboard/sign-out')
 					                
-					              } 
-					        }
-						);
+	// 			// 	              } 
+	// 			// 	        }
+	// 			// 		);	
+	// 		// }
 
-		}.bind(this)
+		
 
+	// }
+	// catch (err) {
+	// 	console.log(err)
+	// }
 
-			// let a : any = document.getElementById("foto_tabungan");
-			// let x : any = document.getElementById("foto_identitas");
-			// let y : any = document.getElementById("foto_npwp");
-			let z : any = document.getElementById("foto_diri");
-			
-			// var file_a =	a.files[0];
-			// var file_x =	x.files[0];
-			// var file_y =	y.files[0];
-			var file_z =	z.files[0];
-			var objectBlob	= new Uint8Array([1,2,3]);
-			var arrayBlob	= objectBlob.buffer;
-			var image_default = new Blob([arrayBlob]);
+	// console.log(this.check_image)
 
-			// console.log(file_a,file_x,file_y,file_z)
-
-			if(file_z == undefined) {
-				file_z = image_default;
-			}
-			// if(file_y == undefined) {
-			// 	file_y = image_default;
-			// }
-			// if(file_x == undefined) {
-			// 	file_x = image_default;
-			// }
-			// if(file_a == undefined) {
-			// 	file_a = image_default;
-			// }
-				readerFileZ.readAsDataURL(file_z);
-				// readerFileY.readAsDataURL(file_y);
-				// readerFileX.readAsDataURL(file_x);
-				// readerFileA.readAsDataURL(file_a);
+	
 				
 			// if(file_z != undefined || file_y != undefined || file_x != undefined || file_a != undefined) {
 			// 	// alert("data tidak valid")
@@ -458,6 +366,191 @@ export class EditComponent implements OnInit {
 
 			// }
 
+		}
+
+	encodeImages(){
+		let readerFileA = new FileReader();
+		readerFileA.onload = function(event, varty) {
+			let fileA = event.target.result.split(',')[1];
+			if(fileA == "AQID") {
+				this.check_image.push(5);
+				this.foto_tabungan = "kosong";
+					if(this.check_image.length < 4) {
+							this.check()
+					}
+					else{
+					return alert(this.check_image);
+						
+					}
+			}
+			else{
+				// this.data.foto_tabungan = fileA;
+				this.foto_tabungan = fileA;
+				this.check_image.push(5)
+				if(this.check_image.length < 4) {
+					this.check()		
+				}
+				else{
+					return alert(this.check_image);
+					
+				}
+			}
+			// for (let i of this.check_image) {
+			// 	let check = [i]
+			// 	if(this.check_image == ) {
+			// 		// code...
+			// 	}
+			// 	console.log(check)
+			// }
+
+			// console.log(this.check_image)
+			// console.log(fileB)
+		}.bind(this)
+
+		let readerFileX = new FileReader();
+  		
+		readerFileX.onload = function(event, varty) {
+			alert("dari file x")
+			let fileX = event.target.result.split(',')[1];
+			// console.log(fileX)
+			if(fileX == "AQID") {
+				this.foto_identitas = null
+				this.check_image.push(3)
+				return;
+			}
+			else{
+				// alert("dari else")
+				// this.data.foto_identitas = fileX;
+				this.foto_identitas = fileX;
+				this.check_image.push(3)
+				return;
+				// console.log(this.foto_identitas)
+			}
+			// console.log(fileA);
+		}.bind(this);
+
+		let readerFileY = new FileReader();
+		readerFileY.onload = function(event, varty) {
+			let fileY = event.target.result.split(',')[1];
+			if(fileY == "AQID") {
+				this.foto_npwp = null
+				this.check_image.push(2)
+				return;
+			}
+			else{
+				this.check_image.push(2)
+				this.foto_npwp = fileY;
+				return;
+				// this.data.foto_npwp = fileY;
+				
+			}
+			
+			// console.log(fileB)
+		}.bind(this)
+
+
+		let readerFileZ = new FileReader();
+		readerFileZ.onload = function(event, varty) {
+			// let dari_file_x = this.foto_identitas
+			// console.log(dari_file_x)
+			let fileZ = event.target.result.split(',')[1];
+				if(fileZ == "AQID") {
+				this.foto_diri = null
+				this.check_image.push(1)
+				return;
+				}
+				if(fileZ != "AQID") {
+				this.foto_diri = fileZ;
+				this.check_image.push(1)
+				// this.data.foto_diri = fileZ;
+				return;
+				}
+				
+
+				
+				// console.log(this.check_image)
+				// this.check_image.push(4)
+				// return	;
+				// this.check()
+				// self(this.check()
+				// EditComponent.call(this.changeProfile())
+				// this.foto_identitas = this.data.foto_identitas;
+				// 	console.log(this.data);
+				// // let image = this.data_image;
+				// this.data_image.foto_diri = this.data.foto_diri
+				// // this.data_image.foto_tabungan = this.data.foto_tabungan
+				// // this.data_image.foto_npwp = this.data.foto_npwp
+				// // image.foto_identitas = this.data.foto_identitas
+				// // console.log(this.data);
+				// console.log("Sedang mengirim data....")
+				
+				// let headers = new Headers({ 
+				//  	'Content-Type': 'application/json',
+				//  	'api_key' : '01b19716dfe44d0e9c656903429c3e9c65d0b243' 
+			 // 	});
+
+			 //    let options = new RequestOptions({ headers: headers });
+
+				// this.http.put('http://masscredit-api.stagingapps.net/user/credential/update-profile',this.data,options)
+				// 		.map(response => response.json())
+				// 		.subscribe(
+				// 			(response : any) => {
+				// 				// for message
+				// 				var kosong:null;
+				// 				var code 		= response.meta.code;
+				// 				var message 	= response.meta.message;
+				// 				if(code == 200) {
+				// 					alert("Profile berhasil diupate")
+				// 					return this.router.navigateByUrl('/dashboard/profile')
+				// 				}
+				// 				else{
+				// 					alert("Profile gagal diupdate")
+				// 				}
+				// 			},
+				// 			(err:any) => {
+				// 	            var error   = JSON.parse(err._body)
+				// 	            var message = error.meta.message
+				// 	              if(message == "unauthorized") {
+				// 	                alert("Maaf session anda telah habis silahkan login kembali")
+				// 	                return this.router.navigateByUrl('/dashboard/sign-out')
+					                
+				// 	              } 
+				// 	        }
+				// 		);
+
+		}.bind(this)
+
+			let a : any = document.getElementById("foto_tabungan");
+			let x : any = document.getElementById("foto_identitas");
+			let y : any = document.getElementById("foto_npwp");
+			let z : any = document.getElementById("foto_diri");
+			
+			var file_a =	a.files[0];
+			var file_x =	x.files[0];
+			var file_y =	y.files[0];
+			var file_z =	z.files[0];
+			var objectBlob	= new Uint8Array([1,2,3]);
+			var arrayBlob	= objectBlob.buffer;
+			var image_default = new Blob([arrayBlob]);
+
+			// console.log(file_a,file_x,file_y,file_z)
+
+			if(file_z == undefined) {
+				file_z = image_default;
+			}
+			if(file_y == undefined) {
+				file_y = image_default;
+			}
+			if(file_x == undefined) {
+				file_x = image_default;
+			}
+			if(file_a == undefined) {
+				file_a = image_default;
+			}
+			readerFileZ.readAsDataURL(file_z);
+			readerFileY.readAsDataURL(file_y);
+			readerFileX.readAsDataURL(file_x);
+			readerFileA.readAsDataURL(file_a);
 	}
 
 
@@ -481,6 +574,7 @@ export class EditComponent implements OnInit {
 
 
 	changeProfile(){
+		console.log(this.data)
 		// alert("dari change profile")
 		// console.log(this.data)
 		// this.check()
@@ -528,84 +622,12 @@ export class EditComponent implements OnInit {
 
 
 	check(){
-		alert("dari check")
-		this.data.foto_identitas = this.foto_identitas
-		this.data.foto_diri = this.foto_diri
-		this.data.foto_tabungan = this.foto_tabungan
-		this.data.foto_npwp = this.foto_npwp
-		console.log(this.data)
-		let headers = new Headers({ 
-		 	'Content-Type': 'application/json',
-		 	'api_key' : '01b19716dfe44d0e9c656903429c3e9c65d0b243' 
-	 	});
-
-	 	let options = new RequestOptions({ headers: headers });
-
-		this.http.put('http://masscredit-api.stagingapps.net/user/credential/update-profile',this.data,options)
-					.map(response => response.json())
-					.subscribe(
-						(response : any) => {
-							// for message
-							var kosong:null;
-							var code 		= response.meta.code;
-							var message 	= response.meta.message;
-							if(code == 200) {
-								alert("Profile berhasil diupate")
-								return this.router.navigateByUrl('/dashboard/profile')
-							}
-							else{
-								alert("Profile gagal diupdate")
-							}
-						},
-						(err:any) => {
-				            var error   = JSON.parse(err._body)
-				            var message = error.meta.message
-				              if(message == "unauthorized") {
-				                alert("Maaf session anda telah habis silahkan login kembali")
-				                return this.router.navigateByUrl('/dashboard/sign-out')
-				                
-				              } 
-				        }
-					);
+		console.log(this.foto_identitas,this.foto_diri,this.foto_tabungan,this.foto_npwp)
+		// this.data.foto_identitas = this.foto_identitas
+		// this.data.foto_diri		 = this.foto_diri
+		// this.data.foto_tabungan	 = this.foto_tabungan
+		// this.data.foto_npwp		 = this.foto_npwp
+		
+	
 	}
 }
-
-	// class encodeFileX {
-	// public readerFileX = new FileReader();
-	// 	this.readerFileX.onload = function(event, varty) {
-	// 		let fileX = event.target.result.split(',')[1];
-	// 		// console.log(fileX)
-	// 		if(fileX == "AQID") {
-	// 			this.data.foto_identitas = null
-	// 			return true;
-	// 		}
-	// 		else{
-	// 			// alert("dari else")
-	// 			this.foto_identitas = fileX;
-	// 			return true;
-	// 			// console.log(this.foto_identitas)
-	// 		}
-	// 		// console.log(fileA);
-	// 	}.bind(this);
-	// }
-// class ClassName {
-	
-// 	public readerFileY: FileReader;
-// 	constructor() {
-// 	}	
-
-		
-// 		readerFileY.onload = function(event, varty) {
-// 			let fileY = event.target.result.split(',')[1];
-// 			if(fileY == "AQID") {
-// 				this.data.foto_npwp = null
-// 			}
-// 			else{
-// 				return this.data.foto_npwp = fileY;
-// 			}
-			
-			
-// 			// console.log(fileB)
-// 		}
-
-// }
