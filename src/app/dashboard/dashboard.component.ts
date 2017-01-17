cc7ac04a2867daa451894b82efcc9812c94642f0
ari_fakhri <ari.fakhri@appschef.com>
2017-01-17 12:05:44 +0700

    delete wrong comment in dashboard.component

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -7,18 +7,8 @@ import { Headers, Http, RequestOptions } from '@angular/http';
 declare var jQuery: any;
 
 @Component({
-  // The selector is what angular internally uses
-  // for `document.querySelectorAll(selector)` in our index.html
-  // where, in this case, selector is the string 'home'
   selector: 'dashboard',  // <dashboard></dashboard>
-  // We need to tell Angular's Dependency Injection which providers are in our app.
-  // providers: [
-  //   Title
-  // ],
-  // Every Angular template is first compiled by the browser before Angular runs it's compiler
   templateUrl: './dashboard.component.html',
-  // styleUrls: ['dashboard.component.css'],
-  // encapsulation: ViewEncapsulation.None,
 })
 export class DashboardComponent {
   
