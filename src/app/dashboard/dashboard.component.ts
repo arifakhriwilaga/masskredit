2c901847312873acfda50e0f67f1053c68d209b0
ari_fakhri <ari.fakhri@appschef.com>
2017-01-18 12:39:53 +0700

    make object for passing data to header component

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,7 @@
-import { Component, ViewEncapsulation }  from '@angular/core';
-import { ActivatedRoute, Router }                from '@angular/router';
+import { Component, ViewEncapsulation, OnInit }  from '@angular/core';
+import { ActivatedRoute, Router }        from '@angular/router';
 import { Headers, Http, RequestOptions } from '@angular/http';
+import { HeaderComponent }               from './shared/header';
 
 
 
@@ -8,57 +9,35 @@ declare var jQuery: any;
 
 @Component({
   selector: 'dashboard',  // <dashboard></dashboard>
-  templateUrl: './dashboard.component.html',
+  templateUrl: './dashboard.component.html'
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit{
   
   constructor(private http : Http, private router:Router) { }
+    // object for pass to header dashboard
+    name : string          = "";
+    last_login : string    = "";
+    user_class : string    = "";
+    user_status : string   = "";
+    profile_image : string = "";
+    investor : string      = "";
+    borrower : string      = "";
+    avg_reviews : string   = "";
+    fund_history : string  = "";
+    account_summary : string = "";
+    is_complete : string = "";
+
   ngOnInit(){
+    this.getProfile();
     jQuery('li > a').click(function() {
       jQuery('li').removeClass();
       jQuery(this).parent().addClass('active');
    
     });
-
-    let token = {
-      'access_token' : JSON.parse(localStorage.getItem("access_token")),
-    }
-    
-    let headers = new Headers({ 
-       'Content-Type': 'application/json',
-       'api_key' : '01b19716dfe44d0e9c656903429c3e9c65d0b243' 
-     });
-
-   
-    console.log("Sedang mengambil data....")
-      let options = new RequestOptions({ headers: headers });
-      this.http.post('http://masscredit-api.stagingapps.net/user/credential/profile',token,options)
-        .map(response => response.json())
-        .subscribe(
-          (response : any) => {
-            // console.log(response);
-            this.profile         = response.data.profile.name;
-            this.account_summary = response.data.account_summary;
-
-            // console.log(this.profile);
-          },
-          (err:any) => {
-            var error   = JSON.parse(err._body)
-            var message = error.meta.message
-              if(message == "unauthorized") {
-                alert("Maaf session anda telah habis silahkan login kembali")
-                return this.router.navigateByUrl('/dashboard/sign-out')
-                
-              }  
-          }
-        );
-
-
-     jQuery('#datepicker').datepicker({
-      format: 'mm/dd/yyyy',
-      // startDate: '3d'
+    // set datepicker
+    jQuery('#datepicker').datepicker({
+      format: 'mm/dd/yyyy'
     });
-
 
     var perShapeGradient = {
         x1: 0,
@@ -66,11 +45,60 @@ export class DashboardComponent {
         x2: 1,
         y2: 0
     };
-
   }
 
   public profile         = [];
-  public account_summary = {};
+  // public account_summary = {};
+  getProfile(){
+    // object for get token
+    let token = {
+      'access_token' : JSON.parse(localStorage.getItem("access_token")),
+    }
+    
+    // set header
+    let headers = new Headers({ 
+       'Content-Type': 'application/json',
+       'api_key' : '01b19716dfe44d0e9c656903429c3e9c65d0b243' 
+     });
+    let options = new RequestOptions({ headers: headers });
+
+   
+    console.log("Sedang mengambil data....")
+    // http request data
+    this.http.post('http://masscredit-api.stagingapps.net/user/credential/profile',token,options)
+      .map(response => response.json())
+      .subscribe(
+        (response : any) => {
+          alert("get profile")
+          // console.log(response);
+          // this.profile         = response.data.profile.name;
+          // this.account_summary = response.data.account_summary;
+
+          this.name           = response.data.profile.name;
+          this.last_login     = response.data.profile.last_login;
+          this.user_class     = response.data.profile.user_score.user_class;
+          this.user_status    = response.data.profile.user_score.user_status;
+          this.profile_image  = response.data.profile.profile_image;
+          this.account_summary= response.data.account_summary.balance;
+          this.investor     = response.data.profile.user_score.investor;
+          this.borrower     = response.data.profile.user_score.borrower;
+          this.avg_reviews  = response.data.profile.user_score.avg_reviews;
+          this.fund_history = response.data.profile.user_score.fund_history;
+          this.is_complete  = response.data.profile.is_complete;
+
+          // console.log(this.profile);
+        },
+        (err:any) => {
+          var error   = JSON.parse(err._body)
+          var message = error.meta.message
+            if(message == "unauthorized") {
+              alert("Maaf session anda telah habis silahkan login kembali")
+              return this.router.navigateByUrl('/dashboard/sign-out')
+              
+            }  
+        }
+      );
+  }
 
 }    
 
