d6919faa3d5ac433db4cab4c54c11783b5461073
ari_fakhri <ari.fakhri@appschef.com>
2017-01-22 17:44:55 +0700

    make component and view for section bank

---
+++ src/app/dashboard/profile/bank/bank.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } 	from '@angular/core';
+import { ProfileComponent }	from './../profile.component';
+
+declare var jQuery:any;
+
+@Component({
+	//moduleId: module.id,
+	selector: 'bank',
+	templateUrl: 'bank.component.html'
+})
+
+export class BankComponent { 
+
+	constructor(private profileComponent : ProfileComponent) {
+	// initial objek in complement data
+	this.data = this.profileComponent.data;
+	}
+
+	public data = { }
+	
+	
+}
