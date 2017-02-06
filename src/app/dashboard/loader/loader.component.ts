e335ea36dbc82264d2c06709b488dd13879af2c7
ari_fakhri <ari.fakhri@appschef.com>
2017-02-06 13:30:10 +0700

    make directory loader for current content dashboard

---
+++ src/app/dashboard/loader/loader.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } 	from '@angular/core';
+import { Headers, Http, RequestOptions }	   from '@angular/http';
+
+declare var jQuery:any;
+
+@Component({
+	//moduleId: module.id,
+	selector: 'loader',
+	templateUrl: 'loader.component.html'
+})
+
+export class LoaderComponent { 
+
+	constructor() {	}
+}
