4f645861ca530a968d7c97db88007e3b197f739e
ari_fakhri <ari.fakhri@appschef.com>
2017-02-08 18:11:19 +0700

    make function for get detail fund_add and make function show modal and hide modal

--- src/app/dashboard/fund/fund_add/fund_add_index/index.component.ts
+++ src/app/dashboard/fund/fund_add/fund_add_index/index.component.ts
@@ -3,6 +3,7 @@ import { ValidationServiceInvestasi }						from './validationservice.component';
 import { Headers, Http, RequestOptions, URLSearchParams }	from '@angular/http';
 import { Router }        									from '@angular/router';
 // import { IndexService }	from './index.service';
+declare var jQuery:any;
 @Component({
 	//moduleId: module.id,
 	selector: 'index',
@@ -143,49 +144,138 @@ export class IndexComponent {
 	 	}
 
 		this.http.post('https://masscredit-api.stagingapps.net/user/fund/get-list',data_get_list_fund, this.options)
-							.map(response => response.json())
-							.subscribe(
-								(response : any) => {
-									// for rr
-									var kosong:null;
-									var code 		= response.meta.code;
-									var message 	= response.meta.message;
-									var funds		= response.data.fund;
-									var current_page= response.data.paging.current_page;
-									var per_page 	= response.data.paging.per_page;
-									var total		= response.data.paging.total;;
-									this.funds 			= funds;
-									this.current_page	= current_page;
-									// this.per_page 		= per_page;
-									// this.total 			= total;
-									console.log(this.current_page)
-									if(code == 200) {
-										console.log("Success get list")
-										// // var convert_string	 = String(total_pagination)
-										// var convert_string	 = total_pagination.valueOf()
-										// array_total 	 = JSON.parse("[" + convert_string +"]");
-										
-									}
-									// console.log(code)
-									// console.log('current page', this.current_page,'per page', this.per_page,'total',this.total)
-									// if(code == 200) {
-									// 	alert("Profile berhasil diupate")
-									// 	// return this.router.navigateByUrl('/dashboard/profile')
-									// }
-									// else{
-									// 	alert("Profile gagal diupdate")
-									// }
-								},
-								(err:any) => {
-						            var error   = JSON.parse(err._body)
-						            var message = error.meta.message
-						              if(message == "unauthorized") {
-						                alert("Maaf session anda telah habis silahkan login kembali")
-						                return this.router.navigateByUrl('/dashboard/sign-out')
-						                
-						              } 
-						        }
-							);
+			.map(response => response.json())
+			.subscribe(
+				(response : any) => {
+					// for rr
+					var kosong:null;
+					var code 		= response.meta.code;
+					var message 	= response.meta.message;
+					var funds		= response.data.fund;
+					var current_page= response.data.paging.current_page;
+					var per_page 	= response.data.paging.per_page;
+					var total		= response.data.paging.total;;
+					this.funds 			= funds;
+					this.current_page	= current_page;
+					// this.per_page 		= per_page;
+					// this.total 			= total;
+					// console.log(this.current_page)
+					if(code == 200) {
+						// console.log("Success get list")
+						// // var convert_string	 = String(total_pagination)
+						// var convert_string	 = total_pagination.valueOf()
+						// array_total 	 = JSON.parse("[" + convert_string +"]");
+						
+					}
+					// console.log(code)
+					// console.log('current page', this.current_page,'per page', this.per_page,'total',this.total)
+					// if(code == 200) {
+					// 	alert("Profile berhasil diupate")
+					// 	// return this.router.navigateByUrl('/dashboard/profile')
+					// }
+					// else{
+					// 	alert("Profile gagal diupdate")
+					// }
+				},
+				(err:any) => {
+		            var error   = JSON.parse(err._body)
+		            var message = error.meta.message
+		              if(message == "unauthorized") {
+		                alert("Maaf session anda telah habis silahkan login kembali")
+		                return this.router.navigateByUrl('/dashboard/sign-out')
+		                
+		              } 
+		        }
+			);
+	}
+
+	public loaderDana = 0;
+	public dataDetailDana = {
+		access_token : JSON.parse(localStorage.getItem("access_token")),
+		fund_id : '',
+	}
+
+	public dataDetailBorrower = {};
+	public detail = { };
+	public bank = { };
+	public dataAmount = { }
+
+	showDetailDana(id:any){
+		this.dataDetailDana.fund_id = id;
+		jQuery('#myModal').modal({backdrop: 'static', keyboard: false});
+		// API detail invest
+	    this.http.post('https://masscredit-api.stagingapps.net/user/fund/get-detail',this.dataDetailDana,this.options)
+				.map(response => response.json())
+				.subscribe((response : any) => {
+					console.log(response)
+					let data = response.data;
+					let bank_name = response.data.bank;
+
+					if(bank_name == 1) {
+						this.bank = "BCA";
+					}
+					if(bank_name == 2) {
+						this.bank = "Mandiri";
+					}
+					if(bank_name == 3) {
+						this.bank = "Danamon";
+					}
+					if(bank_name == 4) {
+						this.bank = "Sinarmas";
+					}
+					if(bank_name == 5) {
+						this.bank = "BNI";
+					}
+					if(bank_name == 6) {
+						this.bank = "Niaga";
+					}
+					if(bank_name == 'undefined') {
+						this.bank = "";
+					}
+					this.dataAmount = response.data.amount;
+					this.delimiterAmount(this.dataAmount);
+
+					this.detail = data
+					// console.log(data);
+					// this.image = response.data.images[0];
+					// console.log(this.image);
+					
+					// console.log(this.detail);
+				});	
+	}
+
+	delimiterAmount(dataAmount:any){
+		try{
+			// condition make delimiter
+			var _minus = false;
+			var b:any = dataAmount.toString();
+			if (b<0) _minus = true;
+				b=b.replace(".","");
+				b=b.replace("-","");
+				let c = "";
+				let panjang = b.length;
+				let j = 0;
+			for (let i = panjang; i > 0; i--){
+				j = j + 1;
+				if (((j % 3) == 1) && (j != 1)){
+					c = b.substr(i-1,1) + "." + c;
+					// console.log(c)
+				} else {
+					c = b.substr(i-1,1) + c;
+				}
+			}
+			if (_minus) c = "-" + c ;
+			let idr = "Rp.";
+			this.dataAmount = idr.concat(c);
+		}finally{
+			this.loaderDana = 1;
+			return true;
+		}
+	}
+
+	hideDetailDana(){
+		jQuery('#myModal').modal('toggle');
+		this.loaderDana = 0;
 	}
 
 
