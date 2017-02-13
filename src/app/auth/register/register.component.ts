5fdd35c0216920a289dfc182a569c7c0a7571433
ari_fakhri <ari.fakhri@appschef.com>
2017-02-13 10:00:51 +0700

    - delete some module - delete directive

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,14 +1,8 @@
 import { Component  } 	from '@angular/core';
-// import { ActivatedRoute }   		from '@angular/router';
-// import { RegisterService }			from './register.service';
-
 
 @Component({
-	//moduleId: module.id,
 	selector: 'register',
 	templateUrl: 'register.component.html',
-  // directives: [Step1RegisterComponent, Step2RegisterComponent, Step3RegisterComponent]
 })
 
-// @Directive({ selector: '[step-1]'})
 export class RegisterComponent { }
