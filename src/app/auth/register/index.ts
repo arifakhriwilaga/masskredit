fea978c81c349494f30e9f625453786bc3f5076b
ari_fakhri <ari.fakhri@appschef.com>
2017-02-13 10:04:34 +0700

    add style guide in index register

--- src/app/auth/register/index.ts
+++ src/app/auth/register/index.ts
@@ -1,51 +1,43 @@
-// Module
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule, ApplicationRef, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { RouterModule, Routes, ActivatedRoute } from '@angular/router';
-import { removeNgStyles, createNewHosts, createInputTransfer } from '@angularclass/hmr';
+// module
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { HttpModule } from '@angular/http';
-import { FormsModule, ReactiveFormsModule, FormBuilder } from '@angular/forms';
-import { RegisterService } from './register.service';
-import { CommonModule  }              from '@angular/common';
-// import { JqueryService }                                     from './jquery.register';
-
+import { FormsModule } from '@angular/forms';
 
 // Component Register
-import { RegisterComponent }        from './register.component';
+import { RegisterComponent } from './register.component';
 
 // Component Verify
-import { FinishStep1Component }          from './finish_step_1';
+import { FinishStep1Component } from './finish_step_1';
 
 // Guard Dashboard
-import { AuthGuardVerify }       from './../../authguard/auth-guard-verify.service';
-import { AuthGuardVerifyCode }   from './../../authguard/auth-guard-verify-code.service';
+import { AuthGuardVerify } from './../../authguard/auth-guard-verify.service';
+import { AuthGuardVerifyCode } from './../../authguard/auth-guard-verify-code.service';
 
 // Guard Verify
-import { AuthGuardVerifyCodeHandphone }   from './../../authguard/auth-guard-verify-code-handphone.service';
-import { AuthGuardVerifyHandphone }   from './../../authguard/auth-guard-verify-handphone.service';
-
-
+// import { AuthGuardVerifyCodeHandphone } from './../../authguard/auth-guard-verify-code-handphone.service';
+import { AuthGuardVerifyHandphone } from './../../authguard/auth-guard-verify-handphone.service';
 
 export const routes = [
   { 
     path: '', 
     component: RegisterComponent,
     children : [
-      { path: '', redirectTo: 'verify' },
+      { path: '', redirectTo: 'phone-number' },
       {    
-        path: 'verify', 
-        canActivate : [ AuthGuardVerifyCode],
-        loadChildren: () => System.import('./verify_handphone').then((comp: any) => comp.default) 
+        path: 'phone-number', 
+        // canActivate : [ AuthGuardVerifyCode],
+        loadChildren: () => System.import('./phone-number').then((comp: any) => comp.default) 
       },
       {    
         path: 'verify-code', 
-        canActivate : [ AuthGuardVerifyCode ],
-        loadChildren: () => System.import('./verify_code').then((comp: any) => comp.default) 
+        // canActivate : [ AuthGuardVerifyCode ],
+        loadChildren: () => System.import('./verify-code').then((comp: any) => comp.default) 
       },       
       {    
-        path: 'step-1', 
-        canActivate : [ AuthGuardVerify ],
-        loadChildren: () => System.import('./step_register').then((comp: any) => comp.default) 
+        path: 'step-register', 
+        // canActivate : [ AuthGuardVerify ],
+        loadChildren: () => System.import('./step-register').then((comp: any) => comp.default) 
       },
       {    
         path: 'finish', 
@@ -64,18 +56,18 @@ export const routes = [
   ],
   imports: [ // import Angular's modules
     HttpModule,
-    FormsModule,
     RouterModule.forChild(routes),
-    CommonModule
+    FormsModule,
+    // CommonModule
   ],
   providers: [ // expose our Services and Providers into Angular's dependency injection
-    // AuthResolve,
     AuthGuardVerify,
     AuthGuardVerifyCode,
-    RegisterService,
-    AuthGuardVerifyHandphone,
-    AuthGuardVerifyCodeHandphone,
-    FormBuilder
+    // AuthGuardVerifyHandphone,
+    // AuthGuardVerifyCodeHandphone,
+    // AuthResolve,
+    // RegisterService,
+    // FormBuilder
   ],
 })
 
