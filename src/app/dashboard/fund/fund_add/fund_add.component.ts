efceb2dc6df9039d5631f414ff338633894523c2
ari_fakhri <ari.fakhri@appschef.com>
2017-02-13 10:15:07 +0700

    delete some file unneed

--- src/app/dashboard/fund/fund_add/fund_add.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } 	from '@angular/core';
-
-@Component({
-	//moduleId: module.id,
-	selector: 'fund-add',
-	templateUrl: 'fund_add.component.html'
-})
-
-export class FundAddComponent { }
