7e65903ac150cce4551d2dbd38612dffe26cfb0f
ari_fakhri <ari.fakhri@appschef.com>
2017-02-13 10:10:03 +0700

    delete some modul in index login

--- src/app/auth/login/index.ts
+++ src/app/auth/login/index.ts
@@ -1,31 +1,25 @@
-// Module
-import { NgModule }              from '@angular/core';
-import { RouterModule }          from '@angular/router';
-import { HttpModule }            from '@angular/http';
-import { FormsModule }           from '@angular/forms';
-// import { LocalStorageService }   from 'angular-2-local-storage';
+// module
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
 
-import { LoginComponent }        from './login.component';
+// component login
+import { LoginComponent } from './login.component';
 
-export const routes = [
-  { path:'', component: LoginComponent}
-] 
+export const routes = [{ path:'', component: LoginComponent}] 
 
 @NgModule({
-  declarations: [
-    LoginComponent, // declarations LoginComponent
-  ],
-  imports: [ // import Angular's modules
+  declarations: [LoginComponent],
+  imports: [
+    RouterModule.forChild(routes),
     HttpModule,
     FormsModule,
-    RouterModule.forChild(routes)
   ],
-  providers: [ // expose our Services and Providers into Angular's dependency injection
-    // LocalStorageService,
-  ]
+  providers: [ ]
 })
 
 export default class LoginModule { 
-  public routes = routes;
+  static routes = routes;
 }
 
