b90cde9713f751f272256786fdbf27b06ed3e031
ari_fakhri <ari.fakhri@appschef.com>
2017-02-13 10:06:20 +0700

    move controller login to service and integrate with component login

--- src/app/auth/login/login.service.ts
+++ src/app/auth/login/login.service.ts
@@ -7,11 +7,10 @@ import 'rxjs/add/operator/toPromise';
 import { User } from './login';
 
 declare var jQuery:any
+
 @Injectable ()
 export class LoginService {
 	constructor (private http:Http, private router:Router) { }
-
-	// private user:User;
 
   // declare object url login
 	private loggedinUrl = 'https://masscredit-api.stagingapps.net/user/credential/login';
@@ -26,47 +25,44 @@ export class LoginService {
   private message = {};
   
 	// function login
-	public login(user:any){
+	postLogin(user:any){
 		return this.http.post(this.loggedinUrl, user, this.options)
 			.map(response => response.json())
-			.subscribe(
-				(response:any) => { 
-					var access_token 		= JSON.stringify(response.data.access_token);
-					var code 		= response.meta.code;
-					var getToken = localStorage.getItem("access_token");
-					if(code == "200") {
-						let token = {value: access_token, timestamp: new Date().getTime()}
-						// localStorage.setItem("access_token",JSON.stringify(token));
-						localStorage.setItem("access_token",access_token);
-						// if(getToken == null) {
-						// }else{
-						// 	if(now-setupTime > hours*60*60*1000) {
-						// 		localStorage.clear();
-						// 		localStorage.setItem("access_token",JSON.stringify(access_token));
-						// 	}	
-						// }
-						// localStorage.setItem("access_token",access_token);
-						return this.router.navigateByUrl('/dashboard');
-					}else{
-						alert("Register gagal");
-					}
-				},
-				(err:any) => {
-					var error   = JSON.parse(err._body);
-					var code = error.meta.code;
-					let message = error.meta.message;
-						if(message == "Account not found") {
-							alert("Akun tidak ditemukan.")
-						  var $this = jQuery("#load");
-				      $this.button('reset');
-						}
-						if(message == null) {
-							alert("Akun tidak ditemukan.")
-						  var $this = jQuery("#load");
-				      $this.button('reset');
-						}
+			.subscribe((response:any) => { 
+				let access_token 		= JSON.stringify(response.data.access_token);
+				let code 		= response.meta.code;
+				let getToken = localStorage.getItem("access_token");
+				if(code == "200") {
+					let token = {value: access_token, timestamp: new Date().getTime()}
+					// localStorage.setItem("access_token",JSON.stringify(token));
+					localStorage.setItem("access_token",access_token);
+					// if(getToken == null) {
+					// }else{
+					// 	if(now-setupTime > hours*60*60*1000) {
+					// 		localStorage.clear();
+					// 		localStorage.setItem("access_token",JSON.stringify(access_token));
+					// 	}	
+					// }
+					// localStorage.setItem("access_token",access_token);
+					return this.router.navigateByUrl('/dashboard');
+				}else{
+					alert("Register gagal");
 				}
-			);
+			},(err:any) => {
+				let error   = JSON.parse(err._body);
+				let code = error.meta.code;
+				let message = error.meta.message;
+				let $this = jQuery("#load"); // not available $this on 'let' so create $this on var
+				if(message == "Account not found") {
+					alert("Akun tidak ditemukan.")
+
+		      $this.button('reset');
+				}
+				if(message == null) {
+					alert("Akun tidak ditemukan.")
+		      $this.button('reset');
+				}
+			});
 	}
 }
 
