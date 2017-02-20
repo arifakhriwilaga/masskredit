b9e075ad2bbb1b7905369eb2133143fc3683532f
ari_fakhri <ari.fakhri@appschef.com>
2017-02-20 13:33:08 +0700

    make directory global-service for service auth

---
+++ src/app/global-service/login.service.ts
@@ -0,0 +1,82 @@
+import { Injectable } from '@angular/core';
+import { Headers, Http, RequestOptions } from '@angular/http';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
+
+import 'rxjs/add/operator/toPromise';
+
+import { User } from './login';
+
+declare var jQuery:any
+
+@Injectable ()
+export class LoginService {
+	constructor (private http:Http, private router:Router) { }
+
+  // declare object url login
+	private loggedinUrl = 'https://masscredit-api.stagingapps.net/user/credential/login';
+	
+	// declare headers
+	private headers = new Headers({ 
+	 	'Content-Type': 'application/json',
+	 	'API_KEY' : '01b19716dfe44d0e9c656903429c3e9c65d0b243' 
+	});    
+  private options = new RequestOptions({ headers: this.headers });
+
+  private message = {};
+
+  // url for redirect after user loging
+  dashboardUrl :string = '/dashboard';
+  // object for condition login
+  isLoggedIn: boolean = false;
+
+	// function login
+	// postLogin(user:any): Observable<boolean>{
+	// 	// alert("dari service login")
+	// 	return this.isLoggedIn = true;
+	// 	// console.log(this.isLoggedIn);
+	// 	//  this.http.post(this.loggedinUrl, user, this.options)
+ //  //            .toPromise()
+ //             // .then(this.extractData)
+ //             // .catch(this.handleError);
+	// 	// console.log(this.isLoggedIn)
+	// 	// this.http.post(this.loggedinUrl, user, this.options)
+	// 	// 	.map(response => response.json())
+	// 	// 	.subscribe((response:any) => { 
+	// 	// 		let access_token 		= JSON.stringify(response.data.access_token);
+	// 	// 		let code 		= response.meta.code;
+	// 	// 		let getToken = localStorage.getItem("access_token");
+	// 	// 		if(code == "200") {
+	// 	// 			let token = {value: access_token, timestamp: new Date().getTime()}
+	// 	// 			// localStorage.setItem("access_token",JSON.stringify(token));
+	// 	// 			localStorage.setItem("access_token",access_token);
+	// 	// 			// if(getToken == null) {
+	// 	// 			// }else{
+	// 	// 			// 	if(now-setupTime > hours*60*60*1000) {
+	// 	// 			// 		localStorage.clear();
+	// 	// 			// 		localStorage.setItem("access_token",JSON.stringify(access_token));
+	// 	// 			// 	}	
+	// 	// 			// }
+	// 	// 			// localStorage.setItem("access_token",access_token);
+	// 	// 		}else{
+	// 	// 			alert("Register gagal");
+	// 	// 		}
+	// 	// 	},(err:any) => {
+	// 	// 		let error   = JSON.parse(err._body);
+	// 	// 		let code = error.meta.code;
+	// 	// 		let message = error.meta.message;
+	// 	// 		let $this = jQuery("#load"); // not available $this on 'let' so create $this on var
+	// 	// 		if(message == "Account not found") {
+	// 	// 			alert("Akun tidak ditemukan.")
+
+	// 	//       $this.button('reset');
+	// 	// 		}
+	// 	// 		if(message == null) {
+	// 	// 			alert("Akun tidak ditemukan.")
+	// 	//       $this.button('reset');
+	// 	// 		}
+	// 	// 	});
+	// }
+}
+
+
