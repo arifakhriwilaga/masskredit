c28486df59023a7ba5f6e69aabc8cfeb03aa0999
ari_fakhri <ari.fakhri@appschef.com>
2017-02-22 17:30:41 +0700

    change name object invest to data in verify loan

--- src/app/dashboard/other_user_action/loan/loan_create/verify_component/verify.component.ts
+++ src/app/dashboard/other_user_action/loan/loan_create/verify_component/verify.component.ts
@@ -9,8 +9,8 @@ declare var jQuery:any;
 	<form class="form-horizontal clearfix row" name="confirmInvestForm" id="confirmInvestForm">
 	  <div class="col-md-12">
 	    <div class="form-group">
-	          <span class="required">*</span>
-	          <input type="password" name="password" placeholder="Password" class="form-control input-md" id="password" [(ngModel)]="invest.password">
+        <span class="required">*</span>
+        <input type="password" name="password" placeholder="Password" class="form-control input-md" id="password" [(ngModel)]="data.password">
 	    </div>
 	  </div>
 	</form>
@@ -18,7 +18,7 @@ declare var jQuery:any;
 	    <div class="form-group">
 	        <div class="col-md-6 col-center">
 	          <button class="btn btn-default" (click)="cancelConfirmInvest()">Batal</button>
-	          <button class="btn btn-red" (click)="confirmInvest(invest)">Verifikasi</button>
+	          <button class="btn btn-red" (click)="confirmInvest(data)">Verifikasi</button>
 	        </div>
 	    </div>
 	  </div>
@@ -27,7 +27,7 @@ declare var jQuery:any;
 
 export class VerifyComponent implements OnInit{ 
 	constructor(private create:CreateComponent, private router:Router) { }
-	private invest = this.create.loan;
+	private data = this.create.loan;
 	public dataConfirmInvest = 0;
 
 	ngOnInit(){
@@ -41,16 +41,16 @@ export class VerifyComponent implements OnInit{
 	}
 
 	cancelConfirmInvest(){
-		
 		this.router.navigateByUrl("/dashboard/other-user-action/invest");
 	}
 
 	confirmInvest(){
 		if(jQuery("#confirmInvestForm").valid()) {
+			this.create.dataInvest = 1;
 			// this.create.dataDetailInvest = 1;
 			// this.dataConfirmInvest = 1;
 			// this.create.dataInvest = 3;
-			// this.create.sendDataInvest();
+			this.create.postLoan();
 		}
 		else{
 			alert("Harap masukan password");
