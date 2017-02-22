84f3c1ebe3daa9289ab610dcd4602bff78ce1c89
ari_fakhri <ari.fakhri@appschef.com>
2017-02-22 14:21:50 +0700

    delete modul.id in invest

--- src/app/dashboard/other_user_action/invest/invest.component.ts
+++ src/app/dashboard/other_user_action/invest/invest.component.ts
@@ -1,7 +1,6 @@
 import { Component } 		from '@angular/core';
 import { ActivatedRoute }   from '@angular/router';
 @Component({
-	//moduleId: module.id,
 	selector: 'invest',
 	templateUrl: 'invest.component.html'
 })
