ee491935d75c55ac17ce7b99163903537dc5114c
ari_fakhri <ari.fakhri@appschef.com>
2017-02-22 14:08:00 +0700

    neated the code index user_loan_detail

--- src/app/dashboard/user_action/user_loan/user_loan_detail/index.ts
+++ src/app/dashboard/user_action/user_loan/user_loan_detail/index.ts
@@ -1,33 +1,36 @@
-// Module
-import { NgModule }              from '@angular/core';
-import { RouterModule }          from '@angular/router';
-import { HttpModule }            from '@angular/http';
-import { FormsModule }           from '@angular/forms';
+// module
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 // import { LocalStorageService }   from 'angular-2-local-storage';
 
-
-// debugger;
-// Component
-import { DetailComponent }        from './detail.component';
+// component
+import { DetailComponent } from './detail.component';
+import { VerifyComponent }  from './verify_component';
 
 export const routes = [
   { path:'', component: DetailComponent},
+  {    
+    path: 'loan-approved/:id', 
+    loadChildren: () => System.import('./loan_approved').then((comp: any) => comp.default) 
+  },
+  {    
+    path: 'loan-not-been-approved/:id', 
+    loadChildren: () => System.import('./loan_not_been_approved').then((comp: any) => comp.default) 
+  },
 ] 
 
 @NgModule({
-  declarations: [
-    DetailComponent, // declarations LoginComponent
-  ],
-  imports: [ // import Angular's modules
+  declarations: [ DetailComponent ],
+  imports: [
     HttpModule,
     FormsModule,
-    RouterModule.forChild(routes)
+    RouterModule.forChild(routes),
+    CommonModule
   ],
-  // exports: [LoginComponent],
-  providers: [ // expose our Services and Providers into Angular's dependency injection
-    // LocalStorageService,
-    // LoginComponent,
-  ]
+  providers: [ ]
 })
 
 export default class DetailModule { 
