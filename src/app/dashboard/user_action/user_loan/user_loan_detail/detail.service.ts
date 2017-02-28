5be686fd679368f3b04e4b0847d7df1b7f139698
ari_fakhri <ari.fakhri@appschef.com>
2017-02-28 13:43:17 +0700

    delete some code unneed in detail_service

--- src/app/dashboard/user_action/user_loan/user_loan_detail/detail.service.ts
+++ src/app/dashboard/user_action/user_loan/user_loan_detail/detail.service.ts
@@ -1,47 +1,23 @@
-import { Injectable }		from '@angular/core';
-import { Headers, Http }	from '@angular/http';
-import { Observable }     	from 'rxjs/Observable';
-
-// Declare Token
-const localtoken 	= JSON.parse(localStorage.getItem('access_token'));
-const token_tambah 	= '34g35g3';
-
-// URL API
-const createURL = 'https://private-f1c97-masscredit.apiary-mock.com/mobile/user/investloan/new'; 
-const indexURL  = 'https://private-f1c97-masscredit.apiary-mock.com/mobile/user/investment/getlist';
+import { Injectable } from '@angular/core';
+import { Headers, Http } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import { Subject } from 'rxjs/Subject';
 
 @Injectable ()
-export class ConfirmService {
-	private headers  		= new Headers ({'Content-Type' : 'application/json'}); //URL to web API
-	private token = localtoken + token_tambah ;
+export class DetailService {
 
-	constructor (private http:Http) { }
-
-	Index(){
-		debugger;
-		if(this.token == "51ef25d29I34Ln1341451adV351534g35g3") {
-			console.log("Berhasil ari")
-			return this.http
-			.post(indexURL,this.token)
-			.subscribe((data: any) => {
-				var getdata = data.json();
-				console.log(getdata);
-			})
-		}
-		else{
-			console.log("data gagal coy",localtoken);
-		}
-	}
+	private PartFormParentSource = new Subject<string>();
+	private PartFormChildSource = new Subject<string>();
 	
-	Create(data) {
-		// code...
+	Part$ = this.PartFormParentSource.asObservable();
+	Part2$ = this.PartFormChildSource.asObservable();
+
+	sendChild(statusPart1:string){
+		this.PartFormParentSource.next(statusPart1);
 	}
 
-	Update(data){
-		// code...
+	sendParent(statusPart2:string){
+		this.PartFormChildSource.next(statusPart2);
 	}
-
 
 }
-		// console.log(token) 
-		// debugger;
