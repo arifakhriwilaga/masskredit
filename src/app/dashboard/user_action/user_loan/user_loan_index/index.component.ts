3d3013ef998ae6374306726c6bc0e6a04e3bdd9e
ari_fakhri <ari.fakhri@appschef.com>
2017-02-28 13:39:19 +0700

    neated the code in index user_loan_index

--- src/app/dashboard/user_action/user_loan/user_loan_index/index.component.ts
+++ src/app/dashboard/user_action/user_loan/user_loan_index/index.component.ts
@@ -35,52 +35,49 @@ export class IndexComponent {
 		this.access_token,
 		this.options)
 		.map(response => response.json())
-		.subscribe(
-			(response : any) => {
-				// console.log(response);
-				// this.dataListMyInvest = 1
-				if(response.data.loans == '') {
-					this.dataArrayNull = 1;
-				}
-				let code 		= response.meta.code;
-				let message 	= response.meta.message;					
-				this.loans = response.data.loans;
-				for(let i = 0; i < this.loans.length; i++){
-				let dataAmount = this.loans[i]
-				let amount = dataAmount['amount'];
-				// condition make delimiter
-				var _minus = false;
-				var b:any = amount.toString();
-				if (b<0) _minus = true;
-					b=b.replace(".","");
-					b=b.replace("-","");
-					let c = "";
-					let panjang = b.length;
-					let j = 0;
-				for (let i = panjang; i > 0; i--){
-					j = j + 1;
-					if (((j % 3) == 1) && (j != 1)){
-						c = b.substr(i-1,1) + "." + c;
-						// console.log(c)
-					} else {
-						c = b.substr(i-1,1) + c;
-					}
-				}
-				if (_minus) c = "-" + c ;
-				let idr = "Rp.";
-				dataAmount['amount'] = idr.concat(c);
-				}
-				this.dataListMyLoan = 1;
-			},
-			(err:any) => {
-				var error   = JSON.parse(err._body)
-				var message = error.meta.message
-					if(message == "unauthorized") {
-						alert("Maaf session anda telah habis silahkan login kembali")
-						return this.router.navigateByUrl('/dashboard/sign-out')					
-					}
+		.subscribe((response : any) => {
+			// console.log(response);
+			// this.dataListMyInvest = 1
+			if(response.data.loans == '') {
+				this.dataArrayNull = 1;
 			}
-		);
+			let code 		= response.meta.code;
+			let message 	= response.meta.message;					
+			this.loans = response.data.loans;
+			for(let i = 0; i < this.loans.length; i++){
+			let dataAmount = this.loans[i]
+			let amount = dataAmount['amount'];
+			// condition make delimiter
+			var _minus = false;
+			var b:any = amount.toString();
+			if (b<0) _minus = true;
+				b=b.replace(".","");
+				b=b.replace("-","");
+				let c = "";
+				let panjang = b.length;
+				let j = 0;
+			for (let i = panjang; i > 0; i--){
+				j = j + 1;
+				if (((j % 3) == 1) && (j != 1)){
+					c = b.substr(i-1,1) + "." + c;
+					// console.log(c)
+				} else {
+					c = b.substr(i-1,1) + c;
+				}
+			}
+			if (_minus) c = "-" + c ;
+			let idr = "Rp.";
+			dataAmount['amount'] = idr.concat(c);
+			}
+			this.dataListMyLoan = 1;
+		},(err:any) => {
+			var error   = JSON.parse(err._body)
+			var message = error.meta.message
+				if(message == "unauthorized") {
+					alert("Maaf session anda telah habis silahkan login kembali")
+					return this.router.navigateByUrl('/dashboard/sign-out')					
+				}
+		});
 	}
 
 }
