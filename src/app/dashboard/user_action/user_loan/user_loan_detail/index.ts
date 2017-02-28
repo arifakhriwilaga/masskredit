a79244d02c27d45c5a532fd3d5fa4975903b58ad
ari_fakhri <ari.fakhri@appschef.com>
2017-02-28 13:42:31 +0700

    import component [PartComponent,LoanNotApprovedComponent, VerifyComponent] because this is component for approve user

--- src/app/dashboard/user_action/user_loan/user_loan_detail/index.ts
+++ src/app/dashboard/user_action/user_loan/user_loan_detail/index.ts
@@ -4,26 +4,32 @@ import { RouterModule } from '@angular/router';
 import { HttpModule } from '@angular/http';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-// import { LocalStorageService }   from 'angular-2-local-storage';
 
 // component
 import { DetailComponent } from './detail.component';
-import { VerifyComponent }  from './verify_component';
+import { PartComponent } from './part';
+import { LoanNotApprovedComponent } from './part/loan-not-approved';
+import { VerifyComponent } from './part/verify_component';
 
 export const routes = [
   { path:'', component: DetailComponent},
   {    
-    path: 'loan-approved/:id', 
-    loadChildren: () => System.import('./loan_approved').then((comp: any) => comp.default) 
+    path: 'loan-approved/:loanId', 
+    loadChildren: () => System.import('./loan-approved').then((comp: any) => comp.default) 
   },
-  {    
-    path: 'loan-not-been-approved/:id', 
-    loadChildren: () => System.import('./loan_not_been_approved').then((comp: any) => comp.default) 
-  },
+  // {    
+  //   path: 'loan-not-approved/:loanId', 
+  //   loadChildren: () => System.import('./loan-not-approved').then((comp: any) => comp.default) 
+  // },
 ] 
 
 @NgModule({
-  declarations: [ DetailComponent ],
+  declarations: [ 
+    DetailComponent, 
+    PartComponent,
+    LoanNotApprovedComponent,
+    VerifyComponent
+  ],
   imports: [
     HttpModule,
     FormsModule,
