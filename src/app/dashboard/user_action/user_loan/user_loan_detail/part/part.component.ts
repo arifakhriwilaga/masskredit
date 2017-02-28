0548447f6a4884ce673a150655b2ab1d52174f34
ari_fakhri <ari.fakhri@appschef.com>
2017-02-28 13:36:52 +0700

    make directory part [loan_not_approved_component, verify_component]

---
+++ src/app/dashboard/user_action/user_loan/user_loan_detail/part/part.component.ts
@@ -0,0 +1,22 @@
+import { Component,EventEmitter,Input,Output } from '@angular/core';
+
+@Component({
+	selector: 'part',
+	template: '<loan-not-approved  [statusPart]="statusPart" [loanId]="loanId" [investId]="investId" (onHide)="onHide($event)" (statusInvestor1)="statusInvestor1($event)"></loan-not-approved>',
+})
+
+export class PartComponent{ 
+	@Input() statusPart:string;
+	@Input() loanId:string;
+	@Input() investId:string;
+	@Output() statusInvestor = new EventEmitter<string>();
+	@Output() onHide2 = new EventEmitter<string>();
+	onHide(status: string) {
+    this.onHide2.emit(status);
+  }
+
+  statusInvestor1(status:any){
+    this.statusInvestor.emit(status);
+  }
+
+}
