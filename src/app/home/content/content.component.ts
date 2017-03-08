a5596a5cc36369e51833ed1c45bd63c67f83b878
ari_fakhri <ari.fakhri@appschef.com>
2017-03-08 15:27:13 +0700

    move function login to content.component

--- src/app/home/content/content.component.ts
+++ src/app/home/content/content.component.ts
@@ -1,10 +1,32 @@
 import { Component } 	from '@angular/core';
 
+// inject service login
+import { LoginService }	from './../../global-service/login.service';
+
+// inject object login
+import { User } from './login';
+
+// declare object jQuery for usage function jQuery
+declare var jQuery:any
+
 @Component({
-	//moduleId: module.id,
 	selector: 'content',
 	templateUrl: 'content.component.html',
 
 })
 
-export class ContentComponent { }
+export class ContentComponent { 
+	constructor(private loginService : LoginService){ }
+// declare object request
+	private user = { User };
+
+	ngOnInit(){
+		// call function jquery mask
+		jQuery('#username').mask('000-000-000000');
+	}
+	login(user){
+		let $this = jQuery("#load");
+    $this.button('loading');
+		this.loginService.postLogin(user);
+	}
+}
