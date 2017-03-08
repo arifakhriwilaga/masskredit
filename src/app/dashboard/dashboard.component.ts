7356f9d4567f7e49f089a705f1f1c8165f7831f5
ari_fakhri <ari.fakhri@appschef.com>
2017-03-08 15:55:53 +0700

    neated the code on dashboard component

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,20 +1,26 @@
 import { Component, ViewEncapsulation, OnInit }  from '@angular/core';
-import { ActivatedRoute, Router }        from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Headers, Http, RequestOptions } from '@angular/http';
-import { HeaderComponent }               from './shared/header';
-import { Global }                 from './../global.service';
+import { HeaderComponent } from './shared/header';
+
+import { User } from './user';
+
+import { Global } from './../global.service';
+import { DashboardService } from './dashboard.service';
+
 
 declare var jQuery: any;
 
 @Component({
-  selector: 'dashboard',  // <dashboard></dashboard>
+  selector: 'dashboard',
   templateUrl: './dashboard.component.html'
 })
 export class DashboardComponent implements OnInit{  
-  constructor(private http : Http, private router:Router) { 
-    // this.name = this.globalService.data
-    // this.globalService.data = this.name
-  }
+  constructor(
+    private http : Http,
+    private router:Router,
+    private dashboardService:DashboardService
+  ) { }
 
   public data_user : Global;
 
@@ -30,7 +36,10 @@ export class DashboardComponent implements OnInit{
     fund_history : string  = "";
     account_summary : string = "";
     is_complete : string = "";
-    // profile_image : null;
+    total_income:any;
+    total_investment:any;
+    total_loan:any;
+    total_withdrawal:any;
 
   ngOnInit(){
     this.getProfile();
@@ -52,12 +61,12 @@ export class DashboardComponent implements OnInit{
     };
   }
 
-  public profile         = [];
+  public profile:User;
  
   // object for get token
-  private token = {
-    'access_token' : JSON.parse(localStorage.getItem("access_token"))
-  }
+  private token ={ 
+    access_token : JSON.parse(localStorage.getItem("access_token"))
+  };
   
   private profileUrl = "https://masscredit-api.stagingapps.net/user/credential/profile";
   
@@ -69,56 +78,97 @@ export class DashboardComponent implements OnInit{
   private options = new RequestOptions({ headers: this.headers });
 
    
-  getProfile(){
-    this.http.post(this.profileUrl,this.token,this.options)
-      .map(response => response.json())
-      .subscribe((response : any) => {
-        this.profile         = response.data.profile.name;
-        this.account_summary = response.data.account_summary;
+  getProfile(): void{
+    this.dashboardService.getProfile(this.token)
+    .then(dataProfile => {
+       console.log(dataProfile)
+      this.name = dataProfile.profile.name;
+      this.last_login = dataProfile.profile.last_login;
+      this.profile_image = dataProfile.profile.profile_image;
 
-        this.name = response.data.profile.name;  
-        // this.name           = this.globalService.data;
-        this.last_login     = response.data.profile.last_login;
-        this.user_class     = response.data.profile.user_score.user_class;
-        this.user_status    = response.data.profile.user_score.user_status;
-        this.profile_image  = response.data.profile.profile_image;
-        this.account_summary= response.data.account_summary.balance;
-        var _minus = false;
-        var b:any = this.account_summary.toString();
-        if (b<0) _minus = true;
-          b=b.replace(".","");
-          b=b.replace("-","");
-          let c = "";
-          let panjang = b.length;
-          let j = 0;
-        for (let i = panjang; i > 0; i--){
-          j = j + 1;
-          if (((j % 3) == 1) && (j != 1)){
-            c = b.substr(i-1,1) + "." + c;
-            // console.log(c)
-          } else {
-            c = b.substr(i-1,1) + c;
-          }
-        }
-        if (_minus) c = "-" + c ;
-        let idr = "Rp.";
-        this.account_summary = idr.concat(c);
-        this.investor     = response.data.profile.user_score.investor;
-        this.borrower     = response.data.profile.user_score.borrower;
-        this.avg_reviews  = response.data.profile.user_score.avg_reviews;
-        this.fund_history = response.data.profile.user_score.fund_history;
-        this.is_complete  = response.data.profile.is_complete;
+      this.delimiter(dataProfile.account_summary.balance)
+      
+      this.user_class = dataProfile.profile.user_score.user_class;
+      this.user_status = dataProfile.profile.user_score.user_status;
+      this.investor = dataProfile.profile.user_score.investor;
+      this.borrower = dataProfile.profile.user_score.borrower;
+      this.fund_history = dataProfile.profile.user_score.fund_history;
+    });
+  }
+
+  // getProfile(){
+  //   this.http.post(this.profileUrl,this.token,this.options)
+  //     .map(response => response.json())
+  //     .subscribe((response : any) => {
+  //       console.log(response)
+  //       this.profile         = response.data.profile.name;
+  //       this.account_summary = response.data.account_summary;
+
+  //       this.name = response.data.profile.name;  
+  //       // this.name           = this.globalService.data;
+  //       this.last_login     = response.data.profile.last_login;
+  //       this.profile_image  = response.data.profile.profile_image;
+  //       this.account_summary= response.data.account_summary.balance;
+  //       var _minus = false;
+  //       var b:any = this.account_summary.toString();
+  //       if (b<0) _minus = true;
+  //         b=b.replace(".","");
+  //         b=b.replace("-","");
+  //         let c = "";
+  //         let panjang = b.length;
+  //         let j = 0;
+  //       for (let i = panjang; i > 0; i--){
+  //         j = j + 1;
+  //         if (((j % 3) == 1) && (j != 1)){
+  //           c = b.substr(i-1,1) + "." + c;
+  //           // console.log(c)
+  //         } else {
+  //           c = b.substr(i-1,1) + c;
+  //         }
+  //       }
+  //       if (_minus) c = "-" + c ;
+  //       let idr = "Rp.";
+  //       this.account_summary = idr.concat(c);
+  //       this.user_class     = response.data.profile.user_score.user_class;
+  //       this.user_status    = response.data.profile.user_score.user_status;
+  //       this.investor     = response.data.profile.user_score.investor;
+  //       this.borrower     = response.data.profile.user_score.borrower;
+  //       this.avg_reviews  = response.data.profile.user_score.avg_reviews;
+  //       this.fund_history = response.data.profile.user_score.fund_history;
+  //       this.is_complete  = response.data.profile.is_complete;
         
-        },(err:any) => {
-          var error   = JSON.parse(err._body)
-          var message = error.meta.message
-            if(message == "unauthorized") {
-              alert("Maaf session anda telah habis silahkan login kembali")
-              return this.router.navigateByUrl('/dashboard/sign-out')
+  //       },(err:any) => {
+  //         var error   = JSON.parse(err._body)
+  //         var message = error.meta.message
+  //           if(message == "unauthorized") {
+  //             alert("Maaf session anda telah habis silahkan login kembali")
+  //             return this.router.navigateByUrl('/dashboard/sign-out')
               
-            }  
-          }
-      );
+  //           }  
+  //         }
+  //     );
+  // }
+
+  delimiter(nominal:any){
+    var _minus = false;
+    var b:any = nominal.toString();
+    if (b<0) _minus = true;
+      b=b.replace(".","");
+      b=b.replace("-","");
+      let c = "";
+      let panjang = b.length;
+      let j = 0;
+    for (let i = panjang; i > 0; i--){
+      j = j + 1;
+      if (((j % 3) == 1) && (j != 1)){
+        c = b.substr(i-1,1) + "." + c;
+      } else {
+        c = b.substr(i-1,1) + c;
+      }
+    }
+    if (_minus) c = "-" + c ;
+    let idr = "Rp.";
+    this.account_summary = idr.concat(c);
   }
 }    
 
