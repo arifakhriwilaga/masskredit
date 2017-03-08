f6adf32bcd7e5f3baac5b271b1616ae7eaff9bde
ari_fakhri <ari.fakhri@appschef.com>
2017-03-08 15:42:50 +0700

    slice function service from component to service

--- src/app/dashboard/other_user_action/loan/loan_index/index.service.ts
+++ src/app/dashboard/other_user_action/loan/loan_index/index.service.ts
@@ -1,35 +1,43 @@
-import { Injectable }		from '@angular/core';
-import { Headers, Http }	from '@angular/http';
-import { Observable }     	from 'rxjs/Observable';
-
-// Declare Token
-const localtoken 	= JSON.parse(localStorage.getItem('access_token'));
-const token_tambah 	= '34g35g3';
-
-// URL API 
-const indexURL  = 'https://private-f1c97-masscredit.apiary-mock.com/mobile/user/investment/getlist';
+import { Injectable } from '@angular/core';
+import { Headers, Http, RequestOptions } from '@angular/http';
+import { Loan } from './loan';
+import { Router } from '@angular/router';
 
 @Injectable ()
 export class IndexService {
-	private headers  		= new Headers ({'Content-Type' : 'application/json'});
-	private token = localtoken + token_tambah ;
 
-	constructor (private http:Http) { }
+	constructor (private http:Http, private router:Router) { }
 
-	Index(){
-		debugger;
-		if(this.token == "51ef25d29I34Ln1341451adV351534g35g3") {
-			console.log("Berhasil ari")
-			return this.http
-			.post(indexURL,this.token)
-			.subscribe((data: any) => {
-				var getdata = data.json();
-				console.log(getdata);
-			})
-		}
-		else{
-			console.log("data gagal coy",localtoken);
+	// set header
+	headers = new Headers({ 
+	 	'Content-Type': 'application/json',
+	 	'api_key' : '01b19716dfe44d0e9c656903429c3e9c65d0b243' 
+	});
+
+	options = new RequestOptions({ headers: this.headers });
+
+	private listInvestUrl = "https://masscredit-api.stagingapps.net/user/investment/getlist";
+	
+	private token = JSON.parse(localStorage.getItem("access_token"))
+
+	getListLoans(data:any): Promise<Loan[]>{
+	 	return this.http.post(this.listInvestUrl,data,this.options)
+		.toPromise()
+		.then(response => response.json().data.investments as Loan[])
+		.catch(this.handleError)	
+	}
+
+	handleError(err){
+		var error   = JSON.parse(err._body)
+    var code = error.meta.code
+    var message = error.meta.message
+  	if(message == "unauthorized") {
+			alert("Maaf session anda telah habis silahkan login kembali")
+			return this.router.navigateByUrl('/dashboard/sign-out')					
 		}
 	}
 
+	toCreateLoan(){
+		this.router.navigateByUrl("/dashboard/other-user-action/loan/create");
+	}
 }
