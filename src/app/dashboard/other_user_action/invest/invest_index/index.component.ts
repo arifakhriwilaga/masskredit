57d51c8ec3868e965eae5cf275886987bdf91cb0
ari_fakhri <ari.fakhri@appschef.com>
2017-03-08 15:52:06 +0700

    integrate function getListInvest on component with index service

--- src/app/dashboard/other_user_action/invest/invest_index/index.component.ts
+++ src/app/dashboard/other_user_action/invest/invest_index/index.component.ts
@@ -1,102 +1,75 @@
 import { Component, OnInit } from '@angular/core';
-import { Headers, Http, RequestOptions } from '@angular/http';
-import { Router } from '@angular/router';
 import {Observable} from 'rxjs/Observable';
 
+import { Invest } from './invest';
+
+import { IndexService } from './index.service'
+
 @Component({
-	//moduleId: module.id,
 	selector: 'index',
-	templateUrl: 'index.component.html'
+	templateUrl: 'index.component.html',
+	providers: [IndexService]
 })
 
-
 export class IndexComponent implements OnInit { 
-	constructor(private router:Router, private http:Http) { }
+	constructor(private indexService:IndexService) { }
 	
-	private headers = new Headers({ 
-			'Content-Type': 'application/json',
-			'api_key' : '01b19716dfe44d0e9c656903429c3e9c65d0b243'
-	});
-
-	private options = new RequestOptions({ headers: this.headers });
-	
-	private access_token = {
-		access_token : JSON.parse(localStorage.getItem("access_token"))
+	private data = {
+		access_token : JSON.parse(localStorage.getItem("access_token")),
+		page:1,
+    limit:10
 	}
 
-	private invest = [];
-	// private dataInvest = [];
-	private amount = {};
+	invest : Invest[];
 	private dataListInvest = 0;
 	private dataArrayNull = 0;
 
-	private data: Observable<Array<any>>
 	ngOnInit(){
-		this.getListInvest()
-		// setTimeout(() => {},0.500);
-
+		setTimeout(() => {this.getListInvests()},0.500);
 	}
 
-	listInvestasi() {
-		// return this.investasiservice.Index();
-	}
+	getListInvests(){
+		this.indexService.getListInvests(this.data)
+    .then(dataInvests => {
+    	this.invest = dataInvests;
 
-	getListInvest(){
-		setTimeout( () => {
-		// alert("Hello"); 
-			this.http.post('https://masscredit-api.stagingapps.net/user/loan/getlist',
-			this.access_token,
-			this.options)
-			.map(response => response.json())
-			.subscribe((response : any) => {
-				let code 		= response.meta.code;
-				let message 	= response.meta.message;
-				if(response.data.loans == '') {
-					this.dataArrayNull = 1;
-				}		
-				this.invest = response.data.loans;
-				for(let i = 0; i < this.invest.length; i++){
-					let dataAmount = this.invest[i]
-					let amount = dataAmount['amount'];
-					let imageDefaultProfile = 'assets/img/default_profile.png';
-					if(dataAmount['image_profile'] == '') {
-						dataAmount['image_profile'] = imageDefaultProfile;
-					}
-					// condition make delimiter
-					var _minus = false;
-					var b:any = amount.toString();
-					if (b<0) _minus = true;
-						b=b.replace(".","");
-						b=b.replace("-","");
-						let c = "";
-						let panjang = b.length;
-						let j = 0;
-					for (let i = panjang; i > 0; i--){
-						j = j + 1;
-						if (((j % 3) == 1) && (j != 1)){
-							c = b.substr(i-1,1) + "." + c;
-							// console.log(c)
-						} else {
-							c = b.substr(i-1,1) + c;
-						}
-					}
-					if (_minus) c = "-" + c ;
-					let idr = "Rp.";
-					dataAmount['amount'] = idr.concat(c);
+			if(dataInvests == null) {
+				this.dataArrayNull = 1;
+			}		
+			for(let i = 0; i < this.invest.length; i++){
+				let dataAmount = this.invest[i]
+				let amount = dataAmount['amount'];
+				let imageDefaultProfile = 'assets/img/default_profile.png';
+				if(dataAmount['image_profile'] == '') {
+					dataAmount['image_profile'] = imageDefaultProfile;
 				}
-					this.dataListInvest = 1;
-			},(err:any) => {
-				var error   = JSON.parse(err._body)
-				var message = error.meta.message
-					if(message == "unauthorized") {
-						alert("Maaf session anda telah habis silahkan login kembali")
-						return this.router.navigateByUrl('/dashboard/sign-out')					
+				// condition make delimiter
+				var _minus = false;
+				var b:any = amount.toString();
+				if (b<0) _minus = true;
+					b=b.replace(".","");
+					b=b.replace("-","");
+					let c = "";
+					let panjang = b.length;
+					let j = 0;
+				for (let i = panjang; i > 0; i--){
+					j = j + 1;
+					if (((j % 3) == 1) && (j != 1)){
+						c = b.substr(i-1,1) + "." + c;
+
+					} else {
+						c = b.substr(i-1,1) + c;
 					}
-			}) 
-		},1);
+				}
+				if (_minus) c = "-" + c ;
+				let idr = "Rp.";
+				dataAmount['amount'] = idr.concat(c);
+			}
+				this.dataListInvest = 1;
+    });
 	}
 
 	linkCreateInvest(){
-		this.router.navigateByUrl("/dashboard/other-user-action/invest/create");
+		this.indexService.toCreateInvest();
 	}
 }
