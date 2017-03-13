04e9f956b0d1a1d2289775c394e51339e2d675e7
ari_fakhri <ari.fakhri@appschef.com>
2017-03-13 08:10:51 +0700

    delete route dun-add detail & confirm

--- src/app/dashboard/fund/fund-add/index.ts
+++ src/app/dashboard/fund/fund-add/index.ts
@@ -1,17 +1,12 @@
 // module
-// import { BrowserModule }                                       from '@angular/platform-browser';
 import { NgModule} from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { HttpModule } from '@angular/http';
 import { FormsModule } from '@angular/forms';
 import { CommonModule  } from '@angular/common';
-// import { removeNgStyles, createNewHosts, createInputTransfer } from '@angularclass/hmr';
-// import { RegisterService }                                     from './register.service';
-// import { JqueryService }                                     from './jquery.register';
-
 
 // component fund add
-import { FundAddComponent }        from './fund-add.component';
+import { FundAddComponent } from './fund-add.component';
 
 export const routes = [
   { 
@@ -19,25 +14,13 @@ export const routes = [
     component: FundAddComponent,
     children : [
       { path: '', redirectTo: 'index' },
-      // { path: 'index', component: IndexComponent },
       {    
         path: 'index', 
         loadChildren: () => System.import('./fund-add-index').then((comp: any) => comp.default) 
       },
       {    
         path: 'create', 
-        // canActivate : [ AuthGuardVerifyCode],
         loadChildren: () => System.import('./fund-add-create').then((comp: any) => comp.default) 
-      },
-      {    
-        path: 'confirm/:id', 
-        // canActivate : [ AuthGuardVerifyCode],
-        loadChildren: () => System.import('./fund-add-confirm').then((comp: any) => comp.default) 
-      },
-      {    
-        path: 'detail/:id', 
-        // canActivate : [ AuthGuardVerifyCode],
-        loadChildren: () => System.import('./fund-add-detail').then((comp: any) => comp.default) 
       },
     ]      
   }     
