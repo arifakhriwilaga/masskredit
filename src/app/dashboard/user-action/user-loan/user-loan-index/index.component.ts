5e2bd023553d55e41c4a541140e293fa34baac9e
ari_fakhri <ari.fakhri@appschef.com>
2017-03-13 14:09:34 +0700

    slice function for get loans to service

--- src/app/dashboard/user-action/user-loan/user-loan-index/index.component.ts
+++ src/app/dashboard/user-action/user-loan/user-loan-index/index.component.ts
@@ -1,6 +1,9 @@
-import { Component } from '@angular/core';
-import { Headers, Http, RequestOptions } from '@angular/http';
-import { Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import {Observable} from 'rxjs/Observable';
+
+import { Loans } from './invest';
+
+import { IndexService } from './index.service'
 
 @Component({
 	selector: 'index',
@@ -9,94 +12,79 @@ import { Router } from '@angular/router';
 
 
 export class IndexComponent { 
-	constructor(private router:Router, private http:Http) { }
+	constructor(private indexService:IndexService) { }
 	
-	private headers = new Headers({ 
-			'Content-Type': 'application/json',
-			'api_key' : '01b19716dfe44d0e9c656903429c3e9c65d0b243'
-	});
-
-	private options = new RequestOptions({ headers: this.headers });
-	
-	private access_token = {
-		access_token : JSON.parse(localStorage.getItem("access_token"))
+	private data = {
+		access_token : JSON.parse(localStorage.getItem("access_token")),
+		page:1,
+    limit:10
 	}
 
-	private loans = [];
-	private dataListMyLoan = 0;
-	private dataArrayNull = 0;
+	private loans:Loans[];
+	private statusInvests = 0;
+	private dataInvestsNull = 0;
 
 	ngOnInit(){
 		this.getListInvest();
 	}
 
 	getListInvest(){
-		this.http.post('https://masscredit-api.stagingapps.net/user/myloan/getlist',
-		this.access_token,
-		this.options)
-		.map(response => response.json())
-		.subscribe((response : any) => {
-			// this.dataListMyInvest = 1
-			if(response.data.loans == '') {
-				this.dataArrayNull = 1;
-			}
-			let code 		= response.meta.code;
-			let message 	= response.meta.message;					
-			this.loans = response.data.loans;
-			for(let i = 0; i < this.loans.length; i++){
-			let data = this.loans[i]
-			let type_invest = data['type_invest'];
-			let loan_category = data['loan_category'];
-			if(type_invest == "") {
-				data['type_invest'] = "-";
-			}if(loan_category == "") {
-				data['loan_category'] = "-";
-			}
-			let imageDefault = 'assets/img/default_image.jpg';
-			if(data['image_loan'] == '') {
-				data['image_loan'] = imageDefault;
-			}
-			let amount = data['amount'];
-			// condition make delimiter
-			var _minus = false;
-			var b:any = amount.toString();
-			if (b<0) _minus = true;
-				b=b.replace(".","");
-				b=b.replace("-","");
-				let c = "";
-				let panjang = b.length;
-				let j = 0;
-			for (let i = panjang; i > 0; i--){
-				j = j + 1;
-				if (((j % 3) == 1) && (j != 1)){
-					c = b.substr(i-1,1) + "." + c;
-					// console.log(c)
-				} else {
-					c = b.substr(i-1,1) + c;
+		this.indexService.getListInvests(this.data).then(dataLoans => {
+			try {
+				if(dataLoans.length == 0) {
+					this.dataInvestsNull = 1;
 				}
+									
+				console.log(dataLoans);
+				// for(let i = 0; i < dataLoans.length; i++){
+				// 	let data = dataLoans[i]
+				// 	let type_invest = data['type_invest'];
+				// 	let loan_category = data['loan_category'];
+				// 	if(type_invest == "") {
+				// 		data['type_invest'] = "-";
+				// 	}if(loan_category == "") {
+				// 		data['loan_category'] = "-";
+				// 	}
+				// 	let imageDefault = 'assets/img/default_image.jpg';
+				// 	if(data['image_invest'] == '') {
+				// 		data['image_invest'] = imageDefault;
+				// 	}
+				// 	let amount = data['amount'];
+				// 	// condition make delimiter
+				// 	var _minus = false;
+				// 	var b:any = amount.toString();
+				// 	if (b<0) _minus = true;
+				// 		b=b.replace(".","");
+				// 		b=b.replace("-","");
+				// 		let c = "";
+				// 		let panjang = b.length;
+				// 		let j = 0;
+				// 	for (let i = panjang; i > 0; i--){
+				// 		j = j + 1;
+				// 		if (((j % 3) == 1) && (j != 1)){
+				// 			c = b.substr(i-1,1) + "." + c;
+				// 			// console.log(c)
+				// 		} else {
+				// 			c = b.substr(i-1,1) + c;
+				// 		}
+				// 	}
+				// 	if (_minus) c = "-" + c ;
+				// 	let idr = "Rp.";
+				// 	data['amount'] = idr.concat(c);
+				// }
+			} finally {
+				this.loans = dataLoans;
+				this.statusInvests = 1;
 			}
-			if (_minus) c = "-" + c ;
-			let idr = "Rp.";
-			data['amount'] = idr.concat(c);
-			}
-			this.dataListMyLoan = 1;
-		},(err:any) => {
-			var error   = JSON.parse(err._body)
-			var message = error.meta.message
-				if(message == "unauthorized") {
-					alert("Maaf session anda telah habis silahkan login kembali")
-					return this.router.navigateByUrl('/dashboard/sign-out')					
-				}
 		});
 	}
 
-	detailAddLoan(loanId:number){
-		this.router.navigateByUrl('/dashboard/user-action/user-loan/detail/'+loanId)
+	detailAddLoan(loanId:number){		
+		this.indexService.detailAddLoan(loanId);
 	}
 
 	detailGiveLoan(loanId:number,investorId:number){
-		// console.log(loanId,investorId);
-		this.router.navigateByUrl('/dashboard/user-action/user-loan/detail/'+loanId+'/loan-approved/'+investorId)
+		this.indexService.detailGiveLoan(loanId,investorId);
 	}
 
 }
