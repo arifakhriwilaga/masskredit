0cf1d21abeb26b05fd88317aec1c8a16a5be0008
ari_fakhri <ari.fakhri@appschef.com>
2017-03-13 08:01:28 +0700

    delete component pendanaan & pinjaman, because new design home only one page

--- src/app/home/index.ts
+++ src/app/home/index.ts
@@ -12,8 +12,6 @@ import { HomeComponent } from './home.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { ContentComponent } from './content';
-import { PendanaanComponent } from './pendanaan';
-import { PinjamanComponent } from './pinjaman';
 
 // routes home base
 export const routes = [
@@ -22,9 +20,6 @@ export const routes = [
 	    children: [
 	      { path: '', redirectTo: 'content' },
 	      { path: 'content', component: ContentComponent },
-	      { path: 'pendanaan', component: PendanaanComponent },
-	      { path: 'pinjaman', component: PinjamanComponent }, 
-
 	    ]
   }
 ];
@@ -35,8 +30,6 @@ export const routes = [
     FooterComponent,
     HeaderComponent,
     ContentComponent,
-    PendanaanComponent,
-    PinjamanComponent
   ],
   imports: [
     CommonModule,
