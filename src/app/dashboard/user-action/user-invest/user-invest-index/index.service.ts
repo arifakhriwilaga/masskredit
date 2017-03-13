2f7a5d81fd3358da0ec39277253a7648f979a1db
ari_fakhri <ari.fakhri@appschef.com>
2017-03-13 14:11:52 +0700

    make service get invests user-invest

--- src/app/dashboard/user-action/user-invest/user-invest-index/index.service.ts
+++ src/app/dashboard/user-action/user-invest/user-invest-index/index.service.ts
@@ -1,35 +1,47 @@
-import { Injectable }		from '@angular/core';
-import { Headers, Http }	from '@angular/http';
-import { Observable }     	from 'rxjs/Observable';
-
-// Declare Token
-const localtoken 	= JSON.parse(localStorage.getItem('access_token'));
-const token_tambah 	= '34g35g3';
-
-// URL API 
-const indexURL  = 'https://private-f1c97-masscredit.apiary-mock.com/mobile/user/investment/getlist';
+import { Injectable } from '@angular/core';
+import { Headers, Http, RequestOptions } from '@angular/http';
+import { Invest } from './invest';
+import { Router } from '@angular/router';
 
 @Injectable ()
 export class IndexService {
-	private headers  		= new Headers ({'Content-Type' : 'application/json'});
-	private token = localtoken + token_tambah ;
 
-	constructor (private http:Http) { }
+	constructor (private http:Http, private router:Router) { }
 
-	Index(){
-		debugger;
-		if(this.token == "51ef25d29I34Ln1341451adV351534g35g3") {
-			console.log("Berhasil ari")
-			return this.http
-			.post(indexURL,this.token)
-			.subscribe((data: any) => {
-				var getdata = data.json();
-				console.log(getdata);
-			})
-		}
-		else{
-			console.log("data gagal coy",localtoken);
+	// set header
+	headers = new Headers({ 
+	 	'Content-Type': 'application/json',
+	 	'api_key' : '01b19716dfe44d0e9c656903429c3e9c65d0b243' 
+	});
+
+	options = new RequestOptions({ headers: this.headers });
+
+	private listInvestUrl = "https://masscredit-api.stagingapps.net/user/myinvestment/getlist";
+	
+	private token = JSON.parse(localStorage.getItem("access_token"))
+
+	getListInvests(data:any): Promise<Invest[]>{
+	 	return this.http.post(this.listInvestUrl,data,this.options)
+		.toPromise()
+		.then(response => response.json().data.investments as Invest[])
+		.catch(this.handleError)	
+	}
+
+	handleError(err){
+		var error   = JSON.parse(err._body)
+    var code = error.meta.code
+    var message = error.meta.message
+  	if(message == "unauthorized") {
+			alert("Maaf session anda telah habis silahkan login kembali")
+			return this.router.navigateByUrl('/dashboard/sign-out')					
 		}
 	}
 
+	detailAddInvest(investId:number){
+		this.router.navigateByUrl('/dashboard/user-action/user-invest/detail/'+investId)
+	}
+
+	detailGiveInvest(investId:number,borrowerId:number){
+		this.router.navigateByUrl('/dashboard/user-action/user-invest/detail/'+investId+'/loan-approved/'+borrowerId)
+	}
 }
