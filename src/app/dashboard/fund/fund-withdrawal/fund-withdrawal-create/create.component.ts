33b0a165501ff62226357227b614c404636da322
ari_fakhri <ari.fakhri@appschef.com>
2017-03-13 08:23:51 +0700

    slice function request getNoreference, getProfile and postFundWithdrawal on index.component because follow style guide angular

--- src/app/dashboard/fund/fund-withdrawal/fund-withdrawal-create/create.component.ts
+++ src/app/dashboard/fund/fund-withdrawal/fund-withdrawal-create/create.component.ts
@@ -2,8 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup} from '@angular/forms';
 import { Headers, Http, RequestOptions } from '@angular/http';
 import { Router } from '@angular/router';
+
+import { Data } from './data';
+
 import { CreateService } from './create.service';
-// import { ValidationServiceInvestasi }	from './validationservice.component';
 
 declare var jQuery:any;
 
@@ -15,27 +17,16 @@ declare var jQuery:any;
 export class CreateComponent implements OnInit{ 
 	constructor(
 		private router : Router, 
-		private http : Http
+		private http : Http,
+		private createService : CreateService
 	) { }
 
-	public dataFundWithdrawal = 0;
+	public statusDataWithdrawalFund = 0;
 
-	// set headers
-  private headers = new Headers({ 
-		'Content-Type': 'application/json',
-		'api_key' : '01b19716dfe44d0e9c656903429c3e9c65d0b243'
-	})
-	private options = new RequestOptions({ headers: this.headers })
-
-	// get access_token in localstorage
 	access_token = JSON.parse(localStorage.getItem("access_token"));
 	
-	// objek request Get No Reference
-	private data_access_token = {
-		access_token : this.access_token
-	}
+	private dataNoReferenceProfile = { access_token : this.access_token }
 
-	// object request Add Fund
 	public data = {
 		access_token : this.access_token,
   	no_reference : null,
@@ -43,98 +34,50 @@ export class CreateComponent implements OnInit{
   	bank_id : null,
 		no_rekening	: null,
 		amount : null,
-  	// date : null,
-		// other_bank : null
 	}
 
-	// object save data bank when success get data bank on API
-	public banks = [];
 
-	private formConfirm = 0;
-
-	private listData = 0;
-	private lastBank = null;
 	public bank = null;
 
-	// declare object url bank
-	private bankUrl = 'https://masscredit-api.stagingapps.net/master/bank';
-	
-	// declare object url profile
-	private profileUrl = 'https://masscredit-api.stagingapps.net/user/credential/profile';
-	
-	// declare object url no-reference
-	private noreferenceUrl = 'https://masscredit-api.stagingapps.net/user/fund/no-reference';
-
-
 	ngOnInit(){
-		// request get profile
  		 this.getProfile();
- 		 // request get no reference
  		 this.getNoReference();
 	}
 
-	// request get no reference
-  getNoReference(){
-	this.http.post(this.noreferenceUrl,this.data,this.options)
-		.map(response => response.json())
-		.subscribe((response : any) => {
-			var code 		= response.meta.code;
-			var message 	= response.meta.message;
-			var no_reference= response.data.no_reference;
-			this.data.no_reference = no_reference;
-			this.listData = 3;
-			// console.log(this.listData)
-			if(this.listData == 3) {
-				this.dataFundWithdrawal = 1;
-			}
-		},(err:any) => {
-			var error   = JSON.parse(err._body);
-			var message = error.meta.message;
-				if(message == "unauthorized") {
-					alert("Maaf session anda telah habis silahkan login kembali");
-					this.router.navigateByUrl('/dashboard/sign-out');
-				}	
+	getNoReference(){
+		this.createService.getNoReference(this.dataNoReferenceProfile).then(dataNoReference => {
+			this.data.no_reference = dataNoReference;
 		});
   }
 
-  // request get profile
   getProfile(){
-		this.http.post(this.profileUrl,this.data_access_token,this.options)
-      .map(response => response.json())
-      .subscribe((response : any) => {
-        // console.log(response);
-        this.data.nama_lengkap	= response.data.profile.name;
-        let bank	= response.data.profile.complement_user.bank;
-        let no_rekening	= response.data.profile.complement_user.no_rekening;
-        this.data.bank_id = bank;
-        if(bank == 0) {
-        	this.bank = "Belum diisi";
-        	this.data.no_rekening = "Belum diisi";
-        }if(bank == 1) {
-        	this.bank = "BCA";
-        }if(bank == 2) {
-        	this.bank = "Mandiri";
-        }if(bank == 3) {
-        	this.bank = "Danamon";
-        }if(bank == 4) {
-        	this.bank = "Sinarmas";
-        }if(bank == 5) {
-        	this.bank = "BNI";
-        }if(bank == 6) {
-        	this.bank = "Niaga";
-        };
-        if(no_rekening == "") {
-        	this.data.no_rekening = "Belum diisi";
-        }else{
-        	this.data.no_rekening = no_rekening;
-        }
-	    },(err:any) => {
-	      var error   = JSON.parse(err._body);
-	      var message = error.meta.message;
-	      if(message == "unauthorized") {
-	        alert("Maaf session anda telah habis silahkan login kembali");
-	        this.router.navigateByUrl('/dashboard/sign-out');
-	      }  
-	    });
+		this.createService.getProfile(this.dataNoReferenceProfile).then(dataProfile => {
+			let bank = dataProfile.profile.complement_user.bank;
+			let no_rekening = dataProfile.profile.complement_user.no_rekening;
+			if(bank == 0) {
+      	this.bank = "Belum diisi";
+      	this.data.no_rekening = "Belum diisi";
+      }if(bank == 1) {
+      	this.bank = "BCA";
+      }if(bank == 2) {
+      	this.bank = "Mandiri";
+      }if(bank == 3) {
+      	this.bank = "Danamon";
+      }if(bank == 4) {
+      	this.bank = "Sinarmas";
+      }if(bank == 5) {
+      	this.bank = "BNI";
+      }if(bank == 6) {
+      	this.bank = "Niaga";
+      };
+      if(no_rekening == "") {
+      	this.data.no_rekening = "Belum diisi";
+      }else{
+      	this.data.no_rekening = no_rekening;
+      }
+			this.data.nama_lengkap = dataProfile.profile.name;
+			this.data.bank_id = dataProfile.profile.complement_user.bank;
+			this.statusDataWithdrawalFund = 1;
+		})
   }
 }
