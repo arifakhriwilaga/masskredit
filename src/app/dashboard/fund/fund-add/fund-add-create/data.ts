7d1873ac986c73ca0c1a92a86749274776f3b9ac
ari_fakhri <ari.fakhri@appschef.com>
2017-03-13 07:58:15 +0700

    make class data for fund-add-create, because follow style guide angular

---
+++ src/app/dashboard/fund/fund-add/fund-add-create/data.ts
@@ -0,0 +1,11 @@
+export class Data {
+	access_token:any;
+	date:string;
+	no_reference:any;
+	nama_lengkap:any;
+	bank_id:number;
+	no_rekening:any;
+	amount:any;
+	nama_bank_lainnya:string;
+	id_bank_masscredit:number;
+}
