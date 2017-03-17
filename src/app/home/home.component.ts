dc9420a965747900d8c04b2ebda680381d503072
ari_fakhri <ari.fakhri@appschef.com>
2017-03-17 14:40:33 +0700

    neated the code home.component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,34 +1,14 @@
-import { Component, ViewEncapsulation }    from '@angular/core';
-import { ActivatedRoute }                  from '@angular/router';
-
+import { Component, ViewEncapsulation } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 declare var jQuery : any;
 
 @Component({
-
-  // //moduleId: module.id,
-  selector: 'home',  // <home></home>
+  selector: 'home',
   templateUrl: './home.component.html',
-  // styleUrls: ['home.component.css'], //component for styleUrls (Masih gagal)
-  // styles: [require('./home.component.css')] //component for styleUrls (Masih gagal)
-  // encapsulation: ViewEncapsulation.None,
+  // styleUrls: ['./home.component.css'],
+  encapsulation: ViewEncapsulation.None,
 })
 
 
-export class HomeComponent { 
-
-
-	ngOnInit(){
-		// nav
-      jQuery("document").ready(function($){
-      var headNav = $('.navbar-head');
-       $(window).scroll(function () {
-           if ($(this).scrollTop() > 40) {
-               headNav.addClass("fixed");
-           } else {
-               headNav.removeClass("fixed");
-           } 
-       });
-      });
-	}
-}
+export class HomeComponent { }
