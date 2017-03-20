dea03076d68e761a4fa78cb4d9da3a8919f6fb0e
ari_fakhri <ari.fakhri@appschef.com>
2017-03-20 16:33:56 +0700

    slice function on sign-out component to service sign-out

--- src/app/dashboard/sign-out/sign-out.component.ts
+++ src/app/dashboard/sign-out/sign-out.component.ts
@@ -1,25 +1,55 @@
-import { Component } 				from '@angular/core';
-import { ActivatedRoute, Router }   from '@angular/router';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { SignOutService } from './sign-out.service';
 
 @Component({
-	//moduleId: module.id,
 	selector: 'sign-out',
 	template: '',
-	
+	providers: [SignOutService]
 })
 
-
-
-export class SignOutComponent { 
-	constructor(private router : Router) { }
+export class SignOutComponent implements AfterViewInit{ 
+	constructor(private router : Router, private signOutService:SignOutService) { }
 
 	ngAfterViewInit() {
-		this.router.navigateByUrl('/home/content');
-		// localStorage.removeItem("access_token");
-		// localStorage.removeItem("verify_handphone");
-		// localStorage.removeItem("access_code");
-		// localStorage.removeItem("access");
-
+		this.signOut();
 	}
+	
+	access_token = JSON.parse(localStorage.getItem("access_token"));
+	
+	dataSignOut = {
+		access_token : this.access_token
+	}
+	
+	signOut(){
+		this.signOutService.signOutUser(this.dataSignOut).then(dataResponse => {
+			let message = dataResponse.meta.message;
+	    let code = JSON.stringify(dataResponse.meta.code);
+	    let data = dataResponse.data;
+
+	    if(code.charAt(0) === '4') {
+	      this.handleError();
+	    } if(code.charAt(0) === '2') {
+	      this.handleSuccess();
+	    };
+
+		});
+	}
+
+	handleError(){          
+    this.clearStorage();
+  }
+  
+  handleSuccess(){
+    this.clearStorage();
+  }
+
+  clearStorage(){
+		localStorage.removeItem("access_token");
+		localStorage.removeItem("verify_handphone");
+		localStorage.removeItem("access_code");
+		localStorage.removeItem("access");
+    this.router.navigateByUrl('/home');
+  }
 
 }
