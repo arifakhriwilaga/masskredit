c5585edb28e6e04c2b2a950417c26d2b43b247a3
ari_fakhri <ari.fakhri@appschef.com>
2017-03-20 11:49:55 +0700

    slice function for request post create invest to service

--- src/app/dashboard/other-user-action/invest/invest-create/create.service.ts
+++ src/app/dashboard/other-user-action/invest/invest-create/create.service.ts
@@ -1,47 +1,32 @@
-import { Injectable }		from '@angular/core';
-import { Headers, Http }	from '@angular/http';
-import { Observable }     	from 'rxjs/Observable';
+import { Injectable } from '@angular/core';
+import { Headers, Http, RequestOptions } from '@angular/http';
+import { Router } from '@angular/router';
 
-// Declare Token
-const localtoken 	= JSON.parse(localStorage.getItem('access_token'));
-const token_tambah 	= '34g35g3';
-
-// URL API
-const createURL = 'https://private-f1c97-masscredit.apiary-mock.com/mobile/user/investloan/new'; 
-const indexURL  = 'https://private-f1c97-masscredit.apiary-mock.com/mobile/user/investment/getlist';
+import 'rxjs/add/operator/toPromise';
 
 @Injectable ()
-export class InvestasiService {
-	private headers  		= new Headers ({'Content-Type' : 'application/json'}); //URL to web API
-	private token = localtoken + token_tambah ;
-
+export class CreateService {
 	constructor (private http:Http) { }
 
-	Index(){
-		debugger;
-		if(this.token == "51ef25d29I34Ln1341451adV351534g35g3") {
-			console.log("Berhasil ari")
-			return this.http
-			.post(indexURL,this.token)
-			.subscribe((data: any) => {
-				var getdata = data.json();
-				console.log(getdata);
-			})
-		}
-		else{
-			console.log("data gagal coy",localtoken);
-		}
-	}
-	
-	Create(data) {
-		// code...
+	private headers = new Headers({ 
+	 	'Content-Type': 'application/json',
+	 	'API_KEY' : '01b19716dfe44d0e9c656903429c3e9c65d0b243' 
+	});    
+  private options = new RequestOptions({ headers: this.headers });
+
+	simulationUrl = 'https://masscredit-api.stagingapps.net/user/investment/simulate';
+
+	calculationInvest(data:any): Promise<any>{
+		return this.http.post(this.simulationUrl,data,this.options)
+		.toPromise()
+		.then(response => response.json())
+		.catch(this.handleError);
 	}
 
-	Update(data){
-		// code...
-	}
+	handleError(err){
+		var error = JSON.parse(err._body);
+    return error;
+ 	}
 
 
 }
-		// console.log(token) 
-		// debugger;
