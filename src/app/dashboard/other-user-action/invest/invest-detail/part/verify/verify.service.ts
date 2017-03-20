d315fd391c1721c7b7b51b6b4eb8aeeaf0599b41
ari_fakhri <ari.fakhri@appschef.com>
2017-03-20 11:36:11 +0700

    make service for verify loan on detail invest

---
+++ src/app/dashboard/other-user-action/invest/invest-detail/part/verify/verify.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { Headers, Http, RequestOptions } from '@angular/http';
+import { Router } from '@angular/router';
+
+import 'rxjs/add/operator/toPromise';
+
+@Injectable ()
+export class VerifyService {
+	constructor (private http:Http) { }
+
+	private headers = new Headers({ 
+	 	'Content-Type': 'application/json',
+	 	'API_KEY' : '01b19716dfe44d0e9c656903429c3e9c65d0b243' 
+	});    
+  private options = new RequestOptions({ headers: this.headers });
+
+	loanUrl = 'https://masscredit-api.stagingapps.net/other-user/loan/new';
+	createLoan(data:any): Promise<any>{
+	 	return this.http.post(this.loanUrl,data,this.options)
+		.toPromise()
+		.then(response => response.json())
+		.catch(this.handleError);	
+	}
+
+	handleError(err){
+		var error = JSON.parse(err._body);
+    return error;
+ 	}	
+
+
+}
