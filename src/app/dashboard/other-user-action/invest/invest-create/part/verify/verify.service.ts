5268a0073ee8f44a8d1989edc1f47d304211d467
ari_fakhri <ari.fakhri@appschef.com>
2017-03-20 11:38:55 +0700

    make service for create new invest

---
+++ src/app/dashboard/other-user-action/invest/invest-create/part/verify/verify.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { Headers, Http, RequestOptions } from '@angular/http';
+import { Router } from '@angular/router';
+
+import 'rxjs/add/operator/toPromise';
+
+@Injectable ()
+export class VerifyService {
+	constructor (private http:Http) { }
+
+	private headers = new Headers({ 
+	 	'Content-Type': 'application/json',
+	 	'API_KEY' : '01b19716dfe44d0e9c656903429c3e9c65d0b243' 
+	});    
+  private options = new RequestOptions({ headers: this.headers });
+
+	investUrl = 'https://masscredit-api.stagingapps.net/user/investment/new';
+	createInvest(data:any): Promise<any>{
+	 	return this.http.post(this.investUrl,data,this.options)
+		.toPromise()
+		.then(response => response.json())
+		.catch(this.handleError);	
+	}
+
+	handleError(err){
+		var error = JSON.parse(err._body);
+    return error;
+ 	}	
+
+
+}
