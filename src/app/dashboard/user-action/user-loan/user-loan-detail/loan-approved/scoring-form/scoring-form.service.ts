e4cbf738d97a1ee01a129f0805033ecf0ddd1fec
ari_fakhri <ari.fakhri@appschef.com>
2017-03-21 17:15:39 +0700

    make service scoring-form on loan-approved

---
+++ src/app/dashboard/user-action/user-loan/user-loan-detail/loan-approved/scoring-form/scoring-form.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { Headers, Http, RequestOptions } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import { dataDetailInvestor } from './loan-approved';
+
+@Injectable ()
+export class ScoringFormService {
+	
+	constructor (private http:Http) { }
+		// set header
+	headers = new Headers({ 
+	 	'Content-Type': 'application/json',
+	 	'api_key' : '01b19716dfe44d0e9c656903429c3e9c65d0b243' 
+	});
+
+	options = new RequestOptions({ headers: this.headers });
+
+	scoringUrl = 'https://masscredit-api.stagingapps.net/user/rate/rate-other-user';
+	rateUser(data:any): Promise<any>{
+	 	return this.http.post(this.scoringUrl,data,this.options)
+			.toPromise()
+			.then(response => response.json())
+			.catch(this.handleError)
+	}
+
+	handleError(err){
+		let error = JSON.parse(err._body)
+    return error
+	}
+
+
+}
