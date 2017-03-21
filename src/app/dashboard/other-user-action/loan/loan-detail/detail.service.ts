18db53a1c813938c75c89307d9f665ee52fb351a
ari_fakhri <ari.fakhri@appschef.com>
2017-03-21 11:26:57 +0700

    make service for get detail loan

--- src/app/dashboard/other-user-action/loan/loan-detail/detail.service.ts
+++ src/app/dashboard/other-user-action/loan/loan-detail/detail.service.ts
@@ -1,47 +1,39 @@
-import { Injectable }		from '@angular/core';
-import { Headers, Http }	from '@angular/http';
-import { Observable }     	from 'rxjs/Observable';
+import { Injectable } from '@angular/core';
+import { Headers, Http, RequestOptions } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
 
-// Declare Token
-const localtoken 	= JSON.parse(localStorage.getItem('access_token'));
-const token_tambah 	= '34g35g3';
-
-// URL API
-const createURL = 'https://private-f1c97-masscredit.apiary-mock.com/mobile/user/investloan/new'; 
-const indexURL  = 'https://private-f1c97-masscredit.apiary-mock.com/mobile/user/investment/getlist';
+import 'rxjs/add/operator/toPromise';
 
 @Injectable ()
-export class ConfirmService {
-	private headers  		= new Headers ({'Content-Type' : 'application/json'}); //URL to web API
-	private token = localtoken + token_tambah ;
-
+export class DetailService {
 	constructor (private http:Http) { }
+	
+	private headers = new Headers({ 
+	 	'Content-Type': 'application/json',
+	 	'API_KEY' : '01b19716dfe44d0e9c656903429c3e9c65d0b243' 
+	});    
+  private options = new RequestOptions({ headers: this.headers });
 
-	Index(){
-		debugger;
-		if(this.token == "51ef25d29I34Ln1341451adV351534g35g3") {
-			console.log("Berhasil ari")
-			return this.http
-			.post(indexURL,this.token)
-			.subscribe((data: any) => {
-				var getdata = data.json();
-				console.log(getdata);
-			})
-		}
-		else{
-			console.log("data gagal coy",localtoken);
-		}
+	detailLoanUrl = 'https://masscredit-api.stagingapps.net/user/investment/detail';
+	
+	getDetail(data:any): Promise<any>{
+		return this.http.post(this.detailLoanUrl,data,this.options)
+		.toPromise()
+		.then(response => response.json())
+		.catch(this.handleError);
 	}
 	
-	Create(data) {
-		// code...
-	}
+	handleError(err){
+		var error = JSON.parse(err._body);
+    return error;
+ 	}
 
-	Update(data){
-		// code...
+ 	calculationUrl = 'https://masscredit-api.stagingapps.net/user/investment/simulate'
+ 	calculationLoan(data:any): Promise<any>{
+		return this.http.post(this.calculationUrl,data,this.options)
+		.toPromise()
+		.then(response => response.json())
+		.catch(this.handleError);
 	}
-
 
 }
-		// console.log(token) 
-		// debugger;
