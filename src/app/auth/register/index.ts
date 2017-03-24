978a17e8f512a1e2944333409b4c22c9cad7ec51
ari_fakhri <ari.fakhri@appschef.com>
2017-03-24 16:47:32 +0700

    delete route phone-number and verify-code

--- src/app/auth/register/index.ts
+++ src/app/auth/register/index.ts
@@ -7,33 +7,18 @@ import { FormsModule } from '@angular/forms';
 // component register
 import { RegisterComponent } from './register.component';
 
-// component verify
-import { FinishStep1Component } from './finish_step_1';
-
 // guard dashboard
 import { AuthGuardVerify } from './../../auth-guard/auth-guard-verify.service';
 import { AuthGuardVerifyCode } from './../../auth-guard/auth-guard-verify-code.service';
 
 // guard verify
-// import { AuthGuardVerifyCodeHandphone } from './../../authguard/auth-guard-verify-code-handphone.service';
 import { AuthGuardVerifyHandphone } from './../../authguard/auth-guard-verify-handphone.service';
 
 export const routes = [
   { 
     path: '', 
     component: RegisterComponent,
-    children : [
-      { path: '', redirectTo: 'phone-number' },
-      {    
-        path: 'phone-number', 
-        // canActivate : [ AuthGuardVerifyCode],
-        loadChildren: () => System.import('./phone-number').then((comp: any) => comp.default) 
-      },
-      {    
-        path: 'verify-code', 
-        // canActivate : [ AuthGuardVerifyCode ],
-        loadChildren: () => System.import('./verify-code').then((comp: any) => comp.default) 
-      },       
+    children : [   
       {    
         path: 'step-register/:access_code', 
         // canActivate : [ AuthGuardVerify ],
@@ -42,35 +27,19 @@ export const routes = [
       {    
         path: 'new-password/:token',
         loadChildren: () => System.import('./new-password').then((comp: any) => comp.default) 
-      },
-      {    
-        path: 'finish', 
-        // canActivate : [ AuthGuardVerify ],
-        component   : FinishStep1Component, 
-      }, 
+      }
     ]
   }
 ];  
 
 @NgModule({
-  declarations: [
-    RegisterComponent,
-    FinishStep1Component
-
-  ],
+  declarations: [ RegisterComponent ],
   imports: [
     HttpModule,
     RouterModule.forChild(routes),
     FormsModule,
   ],
   providers: [
-    AuthGuardVerify,
-    AuthGuardVerifyCode,
-    // AuthGuardVerifyHandphone,
-    // AuthGuardVerifyCodeHandphone,
-    // AuthResolve,
-    // RegisterService,
-    // FormBuilder
   ],
 })
 
