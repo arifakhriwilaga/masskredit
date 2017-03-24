33e0bdeba8cbb8e6ba766f19d1717877083dbaf6
ari_fakhri <ari.fakhri@appschef.com>
2017-03-24 16:34:13 +0700

    delete depedency injection route on scoring-form.service

--- src/app/dashboard/user-action/user-invest/user-invest-detail/loan_approved/scoring-form/scoring-form.service.ts
+++ src/app/dashboard/user-action/user-invest/user-invest-detail/loan_approved/scoring-form/scoring-form.service.ts
@@ -1,14 +1,13 @@
 import { Injectable } from '@angular/core';
 import { Headers, Http, RequestOptions } from '@angular/http';
-import { Loans } from './invest';
-import { Router } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
+import { dataDetailInvestor } from './loan-approved';
 
 @Injectable ()
 export class ScoringFormService {
-
-	constructor (private http:Http, private router:Router) { }
-
-	// set header
+	
+	constructor (private http:Http) { }
+		// set header
 	headers = new Headers({ 
 	 	'Content-Type': 'application/json',
 	 	'api_key' : '01b19716dfe44d0e9c656903429c3e9c65d0b243' 
@@ -16,19 +15,16 @@ export class ScoringFormService {
 
 	options = new RequestOptions({ headers: this.headers });
 
-	private rateUrl = "https://masscredit-api.stagingapps.net/user/rate/rate-other-user";
-	
-	private token = JSON.parse(localStorage.getItem("access_token"))
-
+	scoringUrl = 'https://masscredit-api.stagingapps.net/user/rate/rate-other-user';
 	rateUser(data:any): Promise<any>{
-	 	return this.http.post(this.rateUrl,data,this.options)
-		.toPromise()
-		.then(response => response.json())
-		.catch(this.handleError)	
+	 	return this.http.post(this.scoringUrl,data,this.options)
+			.toPromise()
+			.then(response => response.json())
+			.catch(this.handleError)
 	}
 
 	handleError(err){
-		let error = JSON.parse(err._body);
-    return error;
+		let error = JSON.parse(err._body)
+    return error
 	}
 }
