569ee739d47c82feac439176583c39926eeb83ca
ari_fakhri <ari.fakhri@appschef.com>
2017-04-03 09:22:18 +0700

    objects change to oe object

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -26,22 +26,24 @@ export class DashboardComponent {
   public data_user : Global;
 
     // object for pass to header dashboard
-    name : string ;
-    last_login : string    = "";
-    user_class : string    = "";
-    user_status : string   = "";
-    profile_image : string = "";
-    investor : string      = "";
-    borrower : string      = "";
-    avg_reviews : string   = "";
-    fund_history : string  = "";
-    account_summary : string = "";
-    is_complete : string = "";
-    total_income:any;
-    total_investment:any;
-    total_loan:any;
-    total_withdrawal:any;
-
+  dataUser = {
+    name : null ,
+    last_login : null ,
+    user_class : null ,
+    user_status : null ,
+    profile_image : null ,
+    investor : null ,
+    borrower : null ,
+    fund_history : null ,
+    account_summary : null ,
+    is_complete : null ,
+    total_income:null,
+    total_investment:null,
+    total_loan:null,
+    total_withdrawal:null,
+    avg_review:null
+  }
+  dataComplete:number;
   ngOnInit(){
     this.getProfile();
   }
@@ -61,7 +63,7 @@ export class DashboardComponent {
       let message = dataProfile.meta.message;
       let code = JSON.stringify(dataProfile.meta.code);
       let data = dataProfile.data;
-
+      // console.log(data)
       if(code.charAt(0) === '4') {
         this.handleError(message);
       } if(code.charAt(0) === '2') {
@@ -78,39 +80,42 @@ export class DashboardComponent {
   }
   
   handleSuccess(data:any){
-    this.name = data.profile.name;
-    this.last_login = data.profile.last_login;
-    this.profile_image = data.profile.profile_image;
-
+    this.dataUser.name = data.profile.name;
+    this.dataUser.last_login = data.profile.last_login;
+    this.dataUser.profile_image = data.profile.profile_image;
+    this.dataUser.user_class = data.profile.user_score.user_class;
+    this.dataUser.user_status = data.profile.user_score.user_status;
+    this.dataUser.investor = data.profile.user_score.investor;
+    this.dataUser.borrower = data.profile.user_score.borrower;
+    this.dataUser.fund_history = data.profile.user_score.fund_history;
+    this.dataUser.avg_review = data.profile.user_score.avg_reviews;
     this.delimiter(data.account_summary.balance)
-    
-    this.user_class = data.profile.user_score.user_class;
-    this.user_status = data.profile.user_score.user_status;
-    this.investor = data.profile.user_score.investor;
-    this.borrower = data.profile.user_score.borrower;
-    this.fund_history = data.profile.user_score.fund_history;
   }
 
   delimiter(nominal:any){
-    var _minus = false;
-    var b:any = nominal.toString();
-    if (b<0) _minus = true;
-      b=b.replace(".","");
-      b=b.replace("-","");
-      let c = "";
-      let panjang = b.length;
-      let j = 0;
-    for (let i = panjang; i > 0; i--){
-      j = j + 1;
-      if (((j % 3) == 1) && (j != 1)){
-        c = b.substr(i-1,1) + "." + c;
-      } else {
-        c = b.substr(i-1,1) + c;
+    try {
+      var _minus = false;
+      var b:any = nominal.toString();
+      if (b<0) _minus = true;
+        b=b.replace(".","");
+        b=b.replace("-","");
+        let c = "";
+        let panjang = b.length;
+        let j = 0;
+      for (let i = panjang; i > 0; i--){
+        j = j + 1;
+        if (((j % 3) == 1) && (j != 1)){
+          c = b.substr(i-1,1) + "." + c;
+        } else {
+          c = b.substr(i-1,1) + c;
+        }
       }
+      if (_minus) c = "-" + c ;
+      let idr = "Rp.";
+      this.dataUser.account_summary = idr.concat(c);
+    } finally {
+      this.dataComplete = 1;
     }
-    if (_minus) c = "-" + c ;
-    let idr = "Rp.";
-    this.account_summary = idr.concat(c);
   }
 }    
 
