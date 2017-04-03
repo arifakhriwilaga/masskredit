9fda7717f4a4571e9fc1bf94fb756a1ba1ef536d
ari_fakhri <ari.fakhri@appschef.com>
2017-04-03 09:20:16 +0700

    make condition for show star on detail payment

--- src/app/dashboard/user-action/user-invest/user-invest-detail/loan_approved/detail-payment/detail-payment.component.ts
+++ src/app/dashboard/user-action/user-invest/user-invest-detail/loan_approved/detail-payment/detail-payment.component.ts
@@ -37,14 +37,79 @@ export class DetailPaymentComponent{
 	
 	loaderDetailInvestor = 0;
 	@Input() dataDetailPayment = {
-		payment_date : null
+		payment_date : null,
+		avg_review_payment : null
 	};
+
+	imagePaymentRate25:number;
+	imagePaymentRate50:number;
+	imagePaymentRate75:number;
+	imagePaymentRate100 = [];
+	imagePaymentRate0 = [];
 
 	ngOnInit(){
 		jQuery('#FormDetailPayment').modal({backdrop: 'static', keyboard: false});
 		if(this.dataDetailPayment != null) {
-			console.log(this.dataDetailPayment)
 			try {
+				let rate = [1,1.5,2,2.5,3,3.5,4,4.5,5];
+				let avarage = Number(this.dataDetailPayment.avg_review_payment);
+				// let avarage = 0;
+				if(avarage === 0) {
+					for (var arithmetic = 5; arithmetic >= 1; arithmetic--) {
+						this.imagePaymentRate0.push(arithmetic);
+					}
+				} else {
+					for(let i of rate) {
+						if(i === avarage) {
+
+							// let data = '3.25'; // for dummy
+							let data = i.toString();
+							let number = Number(data); // object if data rate number not decimal
+							let maxRate = 5;
+							
+							// if(data.match(/^[\d]\.[\d+\d]{2}$/)) {
+							if(data.match(/^[\d]\.\d$/)) {
+								let number = Number(data.split('.')[0]);
+								let numberNull = 4 - number;
+								
+								for (var arithmetic = 1; arithmetic <= numberNull; arithmetic++) {
+									this.imagePaymentRate0.push(arithmetic);
+								}
+								for (var arithmetic = 1; arithmetic <= number; arithmetic++) {
+									this.imagePaymentRate100.push(arithmetic)
+								}
+								
+								if(data.match(/^[\d]\.5/)) { // condition if decimal
+									this.imagePaymentRate50 = 1;
+								} 
+
+								// condition if rate change to 25
+
+
+								// if(data.match(/^[\d]\.25/)) { // condition if decimal
+								// 	this.imageRate25 = 1;
+								
+								// } else if(data.match(/^[\d]\.50/)) { // condition if decimal
+								// 	this.imageRate50 = 1;
+
+								// } else { // condition if decimal
+								// 	this.imageRate75 = 1;
+								// }
+							
+							} else {
+								
+								let numberNull = maxRate - number;
+								for (var arithmetic = 1; arithmetic <= number; arithmetic++) {
+									this.imagePaymentRate100.push(arithmetic)
+								}
+
+								for (var arithmetic = 1; arithmetic <= numberNull; arithmetic++) {
+									this.imagePaymentRate0.push(arithmetic);
+								}
+							}
+						}
+					}
+				}
 				if(this.dataDetailPayment.payment_date == null) {
 					this.dataDetailPayment.payment_date = '-';
 				} else {
