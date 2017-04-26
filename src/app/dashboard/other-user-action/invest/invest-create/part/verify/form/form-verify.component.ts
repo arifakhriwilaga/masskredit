9d96710ff23dd20487cc0eee48ba5bfc1c3e4656
ari_fakhri <ari.fakhri@appschef.com>
2017-04-26 09:14:14 +0700

    add input text otp

--- src/app/dashboard/other-user-action/invest/invest-create/part/verify/form/form-verify.component.ts
+++ src/app/dashboard/other-user-action/invest/invest-create/part/verify/form/form-verify.component.ts
@@ -7,8 +7,14 @@ declare var jQuery:any;
 @Component({
 	selector: 'form-verify',
 	template: `
-		<form name="confirmInvestForm" id="confirmInvestForm">
-		  <div class="col-md-6">
+		<form name="confirmInvestForm" id="confirmInvestForm" style="margin-left:100px;margin-right:100px;">
+		  <div class="col-md-12">
+		    <div class="form-group">
+          <span class="required">*</span>
+          <input type="text" name="otp" placeholder="Otp" class="form-control input-md" id="otp" [(ngModel)]="dataInvest.otp">
+		    </div>
+	    </div>
+		  <div class="col-md-12">
 		    <div class="form-group">
           <span class="required">*</span>
           <input type="password" name="password" placeholder="Password" class="form-control input-md" id="password" [(ngModel)]="dataInvest.password">
@@ -26,6 +32,7 @@ export class FormVerifyComponent implements OnInit{
 		jQuery('#confirmInvestForm').validate({
 		  rules: {
 		    password: { required: true },
+		    otp: { required: true },
 		  }
 		});
 	}
