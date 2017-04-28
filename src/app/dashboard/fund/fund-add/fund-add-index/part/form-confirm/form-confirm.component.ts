3ced62fb659d3840f8175826e8abeb6d8e73ac9a
ari_fakhri <ari.fakhri@appschef.com>
2017-04-28 14:39:03 +0700

    enable postConfirm

--- src/app/dashboard/fund/fund-add/fund-add-index/part/form-confirm/form-confirm.component.ts
+++ src/app/dashboard/fund/fund-add/fund-add-index/part/form-confirm/form-confirm.component.ts
@@ -87,11 +87,11 @@ export class FormConfirmComponent {
 	}
 
 	confirmFund(){
-		console.log(this.data)
-		// this.confirmService.postConfirm(this.data).then(dataConfirm => {
-		// 	alert("Penambahan berhasil, harap menunggu konfirmasi admin")
-		// 	this.cancelConfirm();
-		// })
+		// console.log(this.data)
+		this.confirmService.postConfirm(this.data).then(dataConfirm => {
+			alert("Penambahan berhasil, harap menunggu konfirmasi admin")
+			this.cancelConfirm();
+		})
 	}
 
 }
